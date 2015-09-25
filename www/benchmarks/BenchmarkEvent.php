d8acc236c83dda2236dd879bee26d3632df3c660
Bo Henriksen <stumpdenmark@hotmail.com>
2015-09-25 10:55:24 +0200

    A simple benchmark (not working)

---
+++ www/benchmarks/BenchmarkEvent.php
@@ -0,0 +1,24 @@
+<?php
+namespace Benchmark;
+require_once './vendor/autoload.php';
+require_once './kbh_backend/library/TestClass.php';
+
+use Athletic\AthleticEvent;
+
+
+class TestClassEvent extends AthleticEvent
+{
+	private $test;
+	public function setUp()
+	{
+		$this->test = new TestClass();
+	}
+
+	/**
+	 * @iterations 1000
+	 */
+	public function test()
+	{
+		$this->test->hello();
+	}
+}
