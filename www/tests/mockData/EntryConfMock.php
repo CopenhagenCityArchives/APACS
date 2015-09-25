6622d5e580d9a8d82a9e5fdf35590d3ed560ee09
Bo Henriksen <stumpdenmark@hotmail.com>
2015-09-25 09:37:43 +0200

    mockData updated

--- www/tests/mockData/EntryConfMock.php
+++ www/tests/mockData/EntryConfMock.php
@@ -5,59 +5,63 @@ function GetEntryConfMock()
 {
 	return array(
 		'required' => true,
-		'dbTableName' => 'indexdata_test',
+		'dbTableName' => 'insert_table',
 		'isMarkable' => true,
 		'countPerEntry' => 1, //'one' (exactly one), 'zero' (it is allowed to have none (0 to many)), 'many' (minimum 1 but no max (1 to many))
 		'fields' => [
-			'firstname' => [
+			[
+				'name' => 'firstname',
 				'type' => 'text', //Could be: text, dropdown (existing data selection mandatory), autocomplete (it is possible to add not already existing data)
-				'name' => 'Fornavn',
+				'guiName' => 'Fornavn',
 				'defaultValue' => false,
 				'placeholder' => 'Fornavn',
 				'helpText' => 'Indtast personens fornavn(e). Alt andet end det sidste efternavn',
 				'helpLink' => 'kbharkiv.dk/wiki/indtast/begravelsesprotokoller/fornavne',
 				'dbFieldName' => 'firstname',
 				'required' => true,
-				'validation' => false, //Ingen specialtegn eller cifre (kun a-å)
+				'validationRegularExpression' => false, //Ingen specialtegn eller cifre (kun a-å)
 				'validationErrorMessage' => 'Fornavn skal indtastes. Ingen specialtegn tilladt.',
 				'maxLength' => 150
 			],
-			'lastname' => [
+			[
+				'name' => 'lastname',
 				'type' => 'text',
-				'name' => 'Efternavn',
+				'guiName' => 'Efternavn',
 				'defaultValue' => false,
 				'placeholder' => 'Efternavn',
 				'helpText' => 'Indtast personens efternavn',
 				'helpLink' => 'kbharkiv.dk/help',
 				'dbFieldName' => 'lastname',
 				'required' => true,
-				'validation' => false,
+				'validationRegularExpression' => false,
 				'validationErrorMessage' => 'Efternavn skal indtastes',				
 				'maxLength' => 150
 			],
-			'birthdate' => [
+			[
+				'name' => 'birthdate',
 				'type' => 'text',
-				'name' => 'Fødselsdato',
+				'guiName' => 'Fødselsdato',
 				'defaultValue' => '01-01-1700',
 				'placeholder' => 'Fødselsdato (dd-mm-åååå)',
 				'helpText' => 'Indtast personens fødselsdato. Ved manglende information, tast "01"',
 				'helpLink' => 'kbharkiv.dk/foedselsdatoer',
 				'dbFieldName' => 'birthdate',
 				'required' => false,
-				'validation' => 'REGEXP for date values between 1800 and 1923',
+				'validationRegularExpression' => false,
 				'validationErrorMessage' => 'Indtast en dato mellem 1800 og 1923 i formatet dd-mm-åååå',
 				'maxLength' => false
 			],
-			'work' => [
+			[
+				'name' => 'work',
 				'type' => 'autocomplete',
-				'name' => 'Stilling(er)',
+				'guiName' => 'Stilling(er)',
 				'defaultValue' => false,
 				'placeholder' => 'Hovedpersonens stillinger',
 				'helpText' => 'Indtast personens stillinger. Adskil med komma.',
 				'helpLink' => 'kbharkiv.dk/indtast/stillinger',
 				'dbFieldName' => 'work',
 				'required' => false,
-				'validation' => false,
+				'validationRegularExpression' => false,
 				'validationErrorMessage' => false,
 				'maxLength' => false
 			]				
