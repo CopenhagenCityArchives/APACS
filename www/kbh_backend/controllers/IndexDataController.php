6231e0aa9d6e9a43d0fe3586f00693eee799f99b
Bo Henriksen <stumpdenmark@hotmail.com>
2015-09-28 14:53:59 +0200

    Added IndexDataController

---
+++ www/kbh_backend/controllers/IndexDataController.php
@@ -0,0 +1,21 @@
+<?php
+
+class IndexDataController extends \Phalcon\Mvc\Controller
+{
+
+	
+	public function insert()
+	{
+				/*
+		Flow:
+			Check user access rights (authorize and authenticate)
+			Get configuration for the current collection and volume
+			Validate input
+			Build insert statement
+			Save data
+			Return response
+		 */
+
+	}
+
+}
