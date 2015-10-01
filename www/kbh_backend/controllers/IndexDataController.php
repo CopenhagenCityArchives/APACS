c84c9d1fa386c0e59d79487bc14d73bba268dea7
Bo Henriksen <stumpdenmark@hotmail.com>
2015-10-01 15:55:54 +0200

    IndexDataController->insert first implementation and test

--- www/kbh_backend/controllers/IndexDataController.php
+++ www/kbh_backend/controllers/IndexDataController.php
@@ -2,9 +2,14 @@
 
 class IndexDataController extends \Phalcon\Mvc\Controller
 {
+	private $config;
 
+	public function onConstruct()
+	{
+		$this->config = $this->getDI()->get('collectionConfigurationLoader');
+	}
 	
-	public function insert()
+	public function insert($entityId)
 	{
 				/*
 		Flow:
@@ -13,9 +18,39 @@ class IndexDataController extends \Phalcon\Mvc\Controller
 			Validate input
 			Build insert statement
 			Save data
+			Update stats
 			Return response
 		 */
+		
+		//if(!$user->hasAccess)
+		//	throw new Exeception("You don't have access to this action");
+		//	
+		//if(!$user->isAuthorized)
+		//	throw new Exception("Unauthorized access!");
 
+        $this->getDI()->set('currentEntityId', function() use ($entityId){
+            return $entityId;
+        });
+
+		$entity = new GenericIndexModel();
+		$dataReceiver = new DataReceiver(new Phalcon\Http\Request());
+
+		$valuesFieldsMap = $dataReceiver->GetDataFromFields('POST', $this->config->getIndexEntity($entityId)['fields']);
+
+		$errorMessage = [];
+
+		if(!$entity->save($valuesFieldsMap)){
+			foreach($entity->getMessages() as $message){
+				$errorMessage[] = $message;
+			}
+
+			if(count($errorMessage) > 0){
+				array_unshift($errorMessage, 'Validateringsfejl!');
+			}
+
+			return false;
+		}
+
+		return true;
 	}
-
 }
