9ba3f3653e976c1c8c17ca26e1c273f29419a83d
Bo Henriksen <stumpdenmark@hotmail.com>
2015-10-01 14:22:11 +0200

    Added more GenricIndexModel tests

--- www/tests/GenericIndexModelTest.php
+++ www/tests/GenericIndexModelTest.php
@@ -7,6 +7,12 @@ class GenericIndexModelTest extends \UnitTestCase {
     public function setUp(\Phalcon\DiInterface $di = NULL, \Phalcon\Config $config = NULL) {
         $di = new \Phalcon\Di\FactoryDefault;
 
+        $di->set('collectionConfigurationLoader', function(){
+            $conf = new CollectionsConfigurationModel();
+            $conf->loadConfig(require('./mockData/EntryConfMock.php'));
+            return $conf;
+        });    
+
         //Test specific database, Phalcon
         $di->set('db', function(){
             return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
@@ -19,16 +25,6 @@ class GenericIndexModelTest extends \UnitTestCase {
             }
         ); 
 
-        $di->set('collectionConfigurationLoader', function(){
-            $conf = new CollectionsConfigurationModel();
-            $conf->loadConfig('./mockData/MockCollectionsConfiguration.php');
-            return $conf;
-        });    
-
-        $di->set('currentEntityId', function(){
-            return 1;
-        });
-
         parent::setUp($di, $config);
     }
     
@@ -40,51 +36,48 @@ class GenericIndexModelTest extends \UnitTestCase {
 
     public function testDatabaseAccess()
     {
-        $this->getDI()->set('tableNameHolder', function(){
-            return 'insert_table';
-        });
 
-        $this->getDI()->set('conf', function(){
-            return [
-                [
-                    'name' => 'firstname2',
-                    'validationRegularExpression' => '/\w{0,}/',
-                    'validationErrorMessage' => 'Skalbestå af mindst ét tegn'
-                ]
-            ];
+        $this->getDI()->set('currentEntityId', function(){
+            return 234;
         });
 
         $model = new GenericIndexModel();
-        $model->firstname = 'firstname1';
-        $model->lastname = 'lastname1';
-        if(!$model->save()){
-            echo ($model->getMessages()[0]->getMessage());
-        }
-        $this->assertEquals(1,count($model->find("firstname = 'firstname1'")), 'should retrieve data from insert_table');
-        
+        $model->firstname = 'firstnameone';
+        $model->lastname = 'lastnameone';
+        if(!$model->save())
+            throw new Exception("could not save data!");    
 
-        $this->getDI()->set('tableNameHolder', function(){
-            return 'insert_table2';
+        $this->assertEquals(1,count($model->find("lastname = '" . $model->lastname . "'")), 'should retrieve data from insert_table');
+    
+    }
+
+    public function testAnotherDatabaseAccess()
+    {
+        $this->getDI()->set('currentEntityId', function(){
+            return 235;
         });
-        $model = new GenericIndexModel();
 
-        $model->firstname2 = 'firstname2';
-        $model->lastname2 = 'lastname2';
-        $model->save();
-        $this->assertEquals(1,count($model->find("lastname2 = 'lastname2'")), 'should retrieve data from insert_table2');
+        $model = new GenericIndexModel();
+        $model->firstname2 = 'firstnametwo';
+        $model->lastname2 = 'lastnamatwo';
+        if(!$model->save())
+            throw new Exception("could not save data!");
+
+        $this->assertEquals(1,count($model->find("lastname2 = '" . $model->lastname2 . "'")), 'should retrieve data from insert_table2');
 
     }
 
     public function testDynamicValidation()
     {
-        $this->getDI()->set('tableNameHolder', function(){
-            return 'insert_table2';
+        $this->getDI()->set('currentEntityId', function(){
+            return 235;
         });
 
         $model = new GenericIndexModel();
 
-        $model->firstname2 = 'firstname2';
-        $model->lastname2 = 'lastname2';
+        $model->firstname2 = '12 123 2';
+        $model->lastname2 = 'lastname';
         $this->assertEquals($model->save(), false, 'should fail validation');
+        $this->assertGreaterThan(0, strlen($model->getMessages()[0]), 'should create error message');
     }
 }
