a6a0a210f81ba54b9cdcee782d8f3dbaac11f385
Bo Henriksen <stumpdenmark@hotmail.com>
2015-10-20 16:22:42 +0200

    Added index to configuration

--- www/kbh_backend/config/CollectionsConfiguration.php
+++ www/kbh_backend/config/CollectionsConfiguration.php
@@ -394,6 +394,84 @@ $collectionsSettings = array(
                 'order' => 1
             )
         )*/
+        'indexes' => [
+            [
+                'id' => 1,
+                'guiName' => 'Grundregistrering',
+                'info' => 'Registrering af basale oplysninger. Senere kommer en udvidet registring af de sidste informationer.',
+                //The entities of the index
+                'entities' => [
+                    [
+                        //The entity
+                        'id' => 234,
+                        'countPerEntry' => 1, //'one' (exactly one), 'zero' (it is allowed to have none (0 to many)), 'many' (minimum 1 but no max (1 to many))
+                        'required' => true,
+                        'dbTableName' => 'insert_table',
+                        'isMarkable' => true,
+                        'guiName' => 'Personer',
+                        'fields' => [
+                            [
+                                'id' => 231,
+                                'name' => 'firstname',
+                                'type' => 'text', //Could be: text, dropdown (existing data selection mandatory), autocomplete (it is possible to add not already existing data)
+                                'guiName' => 'Fornavn',
+                                'defaultValue' => false,
+                                'placeholder' => 'Fornavn',
+                                'helpText' => 'Indtast personens fornavn(e). Alt andet end det sidste efternavn',
+                                'helpLink' => 'kbharkiv.dk/wiki/indtast/begravelsesprotokoller/fornavne',
+                                'dbFieldName' => 'firstname',
+                                'required' => true,
+                                'validationRegularExpression' => false, //Ingen specialtegn eller cifre (kun a-å)
+                                'validationErrorMessage' => 'Fornavn skal indtastes. Ingen specialtegn tilladt.',
+                                'maxLength' => 150
+                            ],
+                            [
+                                'name' => 'lastname',
+                                'type' => 'text',
+                                'guiName' => 'Efternavn',
+                                'defaultValue' => false,
+                                'placeholder' => 'Efternavn',
+                                'helpText' => 'Indtast personens efternavn',
+                                'helpLink' => 'kbharkiv.dk/help',
+                                'dbFieldName' => 'lastname',
+                                'required' => true,
+                                'validationRegularExpression' => false,
+                                'validationErrorMessage' => 'Efternavn skal indtastes',             
+                                'maxLength' => 150
+                            ],
+                            [
+                                'name' => 'birthdate',
+                                'type' => 'text',
+                                'guiName' => 'Fødselsdato',
+                                'defaultValue' => '01-01-1700',
+                                'placeholder' => 'Fødselsdato (dd-mm-åååå)',
+                                'helpText' => 'Indtast personens fødselsdato. Ved manglende information, tast "01"',
+                                'helpLink' => 'kbharkiv.dk/foedselsdatoer',
+                                'dbFieldName' => 'birthdate',
+                                'required' => false,
+                                'validationRegularExpression' => false,
+                                'validationErrorMessage' => 'Indtast en dato mellem 1800 og 1923 i formatet dd-mm-åååå',
+                                'maxLength' => false
+                            ],
+                            [
+                                'name' => 'work',
+                                'type' => 'autocomplete',
+                                'guiName' => 'Stilling(er)',
+                                'defaultValue' => false,
+                                'placeholder' => 'Hovedpersonens stillinger',
+                                'helpText' => 'Indtast personens stillinger. Adskil med komma.',
+                                'helpLink' => 'kbharkiv.dk/indtast/stillinger',
+                                'dbFieldName' => 'work',
+                                'required' => false,
+                                'validationRegularExpression' => false,
+                                'validationErrorMessage' => false,
+                                'maxLength' => false
+                            ]               
+                        ]
+                    ]
+                ]
+            ]
+        ]
     ),
     array(
         'id' => 6,
@@ -818,7 +896,7 @@ $collectionsSettings = array(
                 'order' => 1
             )
         )*/
-    )    
+    )
 );
 
 return $collectionsSettings;
