74bb8dee8a113876ca90ddb19f9fbd616669374b
Bo Henriksen <stumpdenmark@hotmail.com>
2015-11-27 10:29:52 +0100

    Moved bootstrap functionality

---
+++ apacs/lib/bootstrap.php
@@ -0,0 +1,87 @@
+<?php
+
+    use Phalcon\Mvc\Micro\Collection as MicroCollection;
+ 
+    $app = new Phalcon\Mvc\Micro();
+
+    try {       
+        //Register an autoloader
+        $loader = new \Phalcon\Loader();
+        $loader->registerDirs(array(
+            '../../lib/controllers/',
+            '../../lib/models/',
+            '../../lib/library/'
+        ))->register();
+
+        //Create a DI
+        $di = new Phalcon\DI\FactoryDefault();
+
+        require '../../lib/config/config.php';
+
+        //Setup the configuration service
+        $di->setShared('configuration', function() use ($di){
+            //Loading the almighty configuration array
+            return new ConfigurationLoader('../../lib/config/CollectionsConfiguration.php');
+        });
+
+        //Setup the database service
+        $di->set('database', function() use ($di){
+            return new \Phalcon\Db\Adapter\Pdo\Mysql($di->get('config'));
+        });        
+
+        $di->setShared('response', function(){
+            return new  \Phalcon\Http\Response();
+        });
+        
+        //Metadata routes collection
+        $metadata = new MicroCollection();
+
+        //Set the main handler. ie. a controller instance
+        $metadata->setHandler(new MetadataLevelsController());
+        
+        //Collection info
+        $metadata->get('/collections/{collection:[0-9]+}/info', 'displayinfo');
+        $metadata->get('/collections', 'getcollectioninfo');
+        $metadata->get('/collections/{collection:[0-9]+}', 'getcollectioninfo');
+        
+        //Metadata levels
+        $metadata->get('/levels/{collection:[0-9]+}', 'getmetadatalevels');
+        $metadata->get('/levels/{collection:[0-9]+}/{metadatalevel}', 'getmetadatalevels');
+        
+        //Metadata
+        //What about this: $metadata->('/metadata/{collection:[0-9]+}, should get all metadata for all levels?);
+        $metadata->get('/metadata/{collection:[0-9]+}/{metadatalevel}', 'getmetadata');
+        
+        //Object data
+        $metadata->get('/data/{collection:[0-9]+}', 'getobjectdata');
+        
+        //Error reports
+        $metadata->get('/error/{collection:[0-9]+}/{item:[0-9]+}/{error:[0-9]+}','reporterror');
+
+        $app->mount($metadata);
+        
+        //Indexing routes collection
+        $indexing = new MicroCollection();
+        $indexing->setHandler(new CommonInformationsController());
+
+        $indexing->get('/protocols', 'GetProtocols');
+        $indexing->get('/protocols/{protocol:[0-9]+}', 'GetProtocol');
+
+        $app->mount($indexing);
+        
+        //Not found-handling
+        $app->notFound(function () use ($app, $di) {
+            $di->get('response')->setStatusCode(400, "Not Found");
+            $di->get('response')->setContent('<h1>Bad request!</h1>');
+        });        
+
+        $app->handle();
+
+        //Send any responses collected in the controllers
+        $di->get('response')->send();
+
+    } catch(Exception $e) {
+        $app->response->setStatusCode(500, "Server error");
+        $app->response->setContent("Global exception: ". $e->getMessage());
+        $app->response->send();
+    }
