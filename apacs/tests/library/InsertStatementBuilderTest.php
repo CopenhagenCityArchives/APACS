625b7a9b828f975e84a0b937e3d1dfcf7751f16e
Bo Henriksen <stumpdenmark@hotmail.com>
2015-12-03 15:07:39 +0100

    Updated StatementBuilder tests

--- apacs/tests/library/InsertStatementBuilderTest.php
+++ apacs/tests/library/InsertStatementBuilderTest.php
@@ -14,11 +14,16 @@ class InsertStatementBuilderTest extends \UnitTestCase {
 
     public function testBuildInsertQuery()
     {
-    	$qb1 = new InsertStatementBuilder(['dbTableName' => 'tableName', 'fields' => [['name' => 'field1']]]);
+        $tableName = 'tableName';
+        $fields = [['name' => 'field1', 'dbFieldName' => 'field1']];
+
+    	$qb1 = new InsertStatementBuilder($tableName, $fields);
         $qb1->BuildStatement();
     	$this->assertEquals("INSERT INTO tableName (`field1`) VALUES (:field1)", $qb1->statement, 'should build statement with single field');
 
-    	$qb2 = new InsertStatementBuilder(['dbTableName' => 'tableName2', 'fields' => [['name' => 'field1'], ['name' => 'field2']]]);
+        $tableName2 = 'tableName2';
+        $fields2 = [['name' => 'field1', 'dbFieldName' => 'field1'], ['name' => 'field2', 'dbFieldName' => 'field2']];
+    	$qb2 = new InsertStatementBuilder($tableName2, $fields2);
         $qb2->BuildStatement();
     	$this->assertEquals("INSERT INTO tableName2 (`field1`, `field2`) VALUES (:field1, :field2)", $qb2->statement, 'should return an insert query');
     }
