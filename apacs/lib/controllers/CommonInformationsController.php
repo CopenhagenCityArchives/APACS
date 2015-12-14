af3a5fd02badb2c0f901886cb4a203ea35968074
Bo Henriksen <stumpdenmark@hotmail.com>
2015-12-14 23:31:31 +0100

    Added alot of methods to IndexData and CommonInformations controllers
    Most of them are unnecessary...

--- apacs/lib/controllers/CommonInformationsController.php
+++ apacs/lib/controllers/CommonInformationsController.php
@@ -41,31 +41,6 @@ class CommonInformationsController extends \Phalcon\Mvc\Controller
 		$this->response->setJsonContent($confLoader->GetTask($taskId));
 	}	
 
-	public function GetUnit($unitId)
-	{
-		$unit = Units::findFirst([
-			'conditions' => 'id = :unitId:',
-			'bind' => ['unitId' => $unitId]
-		]);
-
-		$result = [];
-		$result = $unit->toArray(Units::$publicFields);
-		$result['tasks'] = $unit->getTasksUnits()->toArray();
-
-		$this->response->setJsonContent($result);
-	}
-
-	public function GetPage($pageId)
-	{
-		$page = Pages::findFirst(['id' => $pageId]);
-
-		$result = [];
-		$result = $page->toArray(Pages::$publicFields);
-		$result['entries'] = $page->getEntries()->toArray();
-
-		$this->response->setJsonContent($result);
-	}
-
 	public function GetUnits()
 	{
 		$request = $this->getDI()->get('request');
@@ -95,12 +70,26 @@ class CommonInformationsController extends \Phalcon\Mvc\Controller
 		$this->response->setJsonContent($results);
 	}
 
+	public function GetUnit($unitId)
+	{
+		$unit = Units::findFirst([
+			'conditions' => 'id = :unitId:',
+			'bind' => ['unitId' => $unitId]
+		]);
+
+		$result = [];
+		$result = $unit->toArray(Units::$publicFields);
+		$result['tasks'] = $unit->getTasksUnits()->toArray();
+
+		$this->response->setJsonContent($result);
+	}	
+
 	public function GetPages()
 	{
 		$request = $this->getDI()->get('request');
 
-		//$collectionId = $request->getQuery('collection_id', null, false);
-		$unitId = $request->getQuery('unit_id', null, false);
+		$unitId = $request->getQuery('unit_id', 'int', false);
+		$pageNumber = $request->getQuery('page_number', 'int', false);
 
 		if(!$unitId)
 		{
@@ -108,22 +97,307 @@ class CommonInformationsController extends \Phalcon\Mvc\Controller
 			return;
 		}
 
+		$conditions = 'concrete_unit_id = ' . $unitId;
+
+		if($pageNumber !== false)
+			$conditions .= ' AND page_number = ' . $pageNumber;
+
 		$resultSet = Pages::find([
-		    'conditions' => 'concrete_unit_id = :unitId:',
-		    'bind' => ['unitId' => $unitId]
+		    'conditions' => $conditions
     	]);
     	
 		$results = [];
-		$i = 0;
+	//If we want entries at the this level, uncomment this:
+	/*	$i = 0;
+
 		while($resultSet->valid()){
 			$results[$i] = array_intersect_key($resultSet->current()->toArray(), array_flip(Pages::$publicFields));
 			$results[$i]['entries'] = $resultSet->current()->getEntries()->toArray();
 			$resultSet->next();
 			$i++;
-		}
+		}*/
+		$results = $resultSet->toArray();
 
 		$this->response->setJsonContent($results);
+	}	
+
+	public function GetPage($pageId)
+	{
+		$page = Pages::findFirst(['id' => $pageId]);
+		$taskId = $this->request->getQuery('task_id', null, false);
+
+		$entriesCondition = [];
+
+		if($taskId !== false)
+			$entriesCondition = "task_id = " . $taskId;
+
+		$result = [];
+		$result = $page->toArray(Pages::$publicFields);
+		$result['entries'] = $page->getEntries($entriesCondition)->toArray();
+
+		$this->response->setJsonContent($result);
+	}	
+
+	public function GetEntries()
+	{
+		$pageId = $this->request->getQuery('page_id', 'int', false);
+		$taskId = $this->request->getQuery('task_id', 'int', false);
+
+		if($pageId == false)
+		{
+			$this->error('page_id must be set');
+			return;
+		}
+
+		$conditions = 'page_id = ' . $pageId;
+
+		if($taskId !== false)
+		{
+			$conditions .= ' AND task_id = ' . $taskId; 
+		}
+
+		$resultSet = Entries::find(['conditions' => $conditions]);
+
+		$this->response->setJsonContent($resultSet->toArray());
 	}
+
+	public function GetEntry($entryId)
+	{
+		$entry = Entries::findFirst(['id' => $entryId]);
+		$task = $entry->getTasks();
+
+		$entities = $task->getEntities();
+
+		$result = [];
+
+		//Go through entities, get values and map them to fields
+		for($i = 0; $i < count($entities); $i++){
+			$entity = $entities [$i];
+
+			//Load entity fields
+			$query = $this->modelsManager->createQuery('SELECT f.* FROM Entities AS e LEFT JOIN EntitiesFields as ef ON e.id = ef.entity_id LEFT JOIN Fields as f ON ef.field_id = f.id WHERE e.id = ' . $entity->id);			
+			$fields  = $query->execute()->toArray();
+			
+			//Instantiate loader for concrete entry values
+			$ge = new GenericEntry($entity->dbTableName, $fields, $this->getDI());
+
+			//Get entries based on the entity
+			$entries = $entity->getEntriesEntities()->toArray();
+
+			$values = [];
+			foreach($entries as $entry)
+			{	
+				//Load data
+				$data = $ge->Load($entry['id']);			
+				
+				//We have the values. Let's map them to the fields
+				foreach($fields as $field){
+					$row = [];
+					$row['fieldname'] = $field['dbFieldName'];
+					$row['value'] = $data[$field['dbFieldName']];
+					$values[] = $row;
+				}				
+			}
+
+			//Setting data (entity info and values)
+			$result[$i] = ['entity_id' => $entity->id, 'entry_id' => $entryId];
+			$result[$i]['values'] = $values;
+		}
+
+		$this->response->setJsonContent($result);
+	}
+
+	public function SaveEntry()
+	{
+
+		$entry = $this->request->getJsonRawBody();
+
+		$postId = $entry->post_id;
+		$taskId = $entry->task_id;
+
+		$concreteEntries = [];
+		
+		$dataIsValid = true;
+		foreach($entry['entity_groups'] as $entityGroup){
+
+			$fieldsMetadata = Entries::findFirst($entityGroup['entity_id'])->getFields()->toArray();
+
+			foreach($entityGroup['entities'] as $entity){
+				$ge = new GenericEntry($fieldsMetadata, $entity['fields'], $this->getDI());
+				if(!$ge->Validate())
+				{
+					$dataIsValid = false;
+				}
+				$concreteEntries[] = $ge;
+			}  
+		}
+
+		//All rigth, we have all entries. Let's validate them!
+		$dataIsValid = true;
+		foreach($concreteEntries as $conEntry)
+		{
+			if(!$conEntry->Validate())
+			{
+				$dataIsValid = false;
+				break;
+			}
+		}
+
+		if(!$dataIsValid)
+
+
+		$task = Tasks::findFirst(['id' => $taskId]);
+
+		$entities = $task->getEntities();
+
+		$result = [];
+
+		//Go through entities, get values and map them to fields
+		for($i = 0; $i < count($entities); $i++){
+			$entity = $entities [$i];
+
+			//Load entity fields
+			$query = $this->modelsManager->createQuery('SELECT f.* FROM Entities AS e LEFT JOIN EntitiesFields as ef ON e.id = ef.entity_id LEFT JOIN Fields as f ON ef.field_id = f.id WHERE e.id = ' . $entity->id);			
+			$fields  = $query->execute()->toArray();
+			
+			//Instantiate loader for concrete entry values
+			$ge = new GenericEntry($entity->dbTableName, $fields, $this->getDI());
+
+			if(!$ge->Save()){
+				$this->error($ge->GetErrorMessages());
+			}
+
+			//Data is saved! Let's put the data in the aggregated array
+			$values = $ge->GetData();
+
+			//Setting data (entity info and values)
+			//TODO: Save the id of the generic entry!
+			$result[$i] = ['entity_id' => $entity->id, 'entry_id' => 1];
+			$result[$i]['values'] = $values;
+		}
+
+		$entry = new Entries();
+		$entry->data = $result;
+		$entry->task_id = $taskId;
+		$entry->post_id = $postId;
+
+		$entry->save();
+
+		$this->response->setJsonContent($entry->toArray());		
+	}
+
+	public function GetEntry_ORG($entryId)
+	{
+		$entry = Entries::findFirst(['id' => $entryId]);	
+
+		$data = $entry->toArray();
+
+		//Lets combine entry values and entity fields!
+		
+		$entryEntities = $entry->getEntriesEntities(['entry_id' => $entryId]);
+		$i = 0;
+		foreach($entryEntities as $entryEntity)
+		{			
+			$entity = $entryEntity->getEntities();
+			
+			$entityData = [];
+			$entityData['entity_id'] = $entity->id;
+			$entityData['entry_id'] = $entryEntity->entry_id;
+			$data['entities'][$i] = $entityData;
+
+			$fields = [];
+			foreach($entity->getEntitiesFields() as $entityField)
+			{
+				$fields[] = $entityField->getFields()->toArray();
+			}
+
+			$ge = new GenericEntry($entity->toArray()['dbTableName'], $fields, $this->getDI());
+			$entryData = $ge->Load($entryId);
+
+			$rows = [];
+			foreach($entryData[0] as $key => $val){
+				foreach($fields as $field){
+					if($field['dbFieldName'] == $key){
+						$newField = [];
+						$newField['fieldname'] = $field['dbFieldName'];
+						$newField['value'] = $val;
+						$newField['id'] = null;
+						$newField['unreadable'] = null;
+						$rows[] = $newField;
+					}
+				}
+			}
+
+			$data['field'][$i]['values'] = $rows;
+			$i++;
+		}
+
+		$this->response->setJsonContent($data);
+	}
+
+	public function CreateEntry()
+	{
+		$taskId = $this->request->getPost('task_id', 'int', false);
+		$pageId = $this->request->getPost('page_id', 'int', false);
+
+		if($taskId == false || $pageId == false)
+		{
+			$this->error('task_id and page_id must be set');
+			return;
+		}
+/*
+		$fields = $this->config->getCollection(5)[0]['indexes'][0]['entities'][0]['fields'];
+		$table = $this->config->getCollection(5)[0]['indexes'][0]['entities'][0]['dbTableName'];
+*/
+		$configLoader = new DBConfigurationLoader();
+		$task = $configLoader->GetTask($taskId);
+
+		$genericEntry = new GenericEntry($table, $fields, $this->getDI());
+		
+		$action = $this->request->getPost('action', 'string', false);
+
+		//Validation only. Do not save
+		if($action == 'validate'){
+			$valid = $genericEntry->PartialValidation();
+			
+			if($valid){
+				$this->response->setStatusCode('400');
+				$this->response->setJsonContent(['message' => 'all fields are okay']);
+			}
+			else{
+				$this->response->setStatusCode('404');
+				$this->response->setJsonContent($genericEntry->GetErrorMessages());
+			}
+
+			return;
+		}
+
+		//Saving data, return error messages if not possible or input is invalid
+		if(!$genericEntry->Save())
+		{
+			$this->response->setStatusCode(401, 'Could not save entry');
+			$this->response->setJsonContent($genericEntry->GetErrorMessages());
+			return;
+		}
+/*
+		//Saving the generic entry
+		//Needed: collection id, task id, page id	
+		$metaInfo = $dataReceiver->GetDataFromFields('POST', ['collection_id', 'task_id', 'page_id']);
+		
+		$entity = new Entries();
+		$entity->collection_id = $metaInfo['collection_id'];
+		$entity->task_id = $metaInfo['task_id'];
+		$entity->page_id = $metaInfo['page_id'];
+		$entity->data = json_encode($genericEntry->GetData(), true);
+
+		if(!$entity->save()){
+			$this->response->setStatusCode(500, 'Could not save meta entry');
+			return $this->response;
+		}*/
+
+		$this->response->setStatusCode(201, 'Data saved');
+		$this->response->setJsonContent($genericEntry->GetData());		
+	}	
 
 	public function ImportUnits()
 	{
@@ -138,7 +412,7 @@ class CommonInformationsController extends \Phalcon\Mvc\Controller
 
 		$type = $request->getPost('type', null, Units::OPERATION_TYPE_CREATE);
 
-		$importer = new UnitsModel();
+		$importer = new Units();
 		$colConfig = $this->getDI()->get('configuration')->getCollection($collectionId)[0];
 
 		if($importer->Import($type, $collectionId, $colConfig['units_id_field'], $colConfig['units_info_field'], $colConfig['units_table'], $colConfig['units_info_condition']))
@@ -165,10 +439,10 @@ class CommonInformationsController extends \Phalcon\Mvc\Controller
 
 		$type = $request->getPost('type', null, Pages::OPERATION_TYPE_CREATE);
 
-		$importer = new PagesModel();
+		$importer = new Pages();
 		$colConfig = $this->getDI()->get('configuration')->getCollection($collectionId)[0];
 
-		if($importer->Import($type, $collectionId, $colConfig['pages_id_field'], $colConfig['pages_unit_id_field'], $colConfig['pages_table'], $colConfig['pages_info_condition']))
+		if($importer->Import($type, $collectionId, $colConfig['pages_id_field'], $colConfig['pages_unit_id_field'], $colConfig['pages_table'], $colConfig['pages_image_url'], $colConfig['pages_info_condition']))
 		{
 			$this->response->setStatusCode('201', 'Content added');
 			$this->response->setJsonContent($importer->GetStatus());
