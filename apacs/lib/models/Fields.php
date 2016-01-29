b4f1b95342eabf68de95e812ad0b8cd72fc74e42
Bo Henriksen <stumpdenmark@hotmail.com>
2016-01-29 18:09:14 +0100

    Prettified Fields

--- apacs/lib/models/Fields.php
+++ apacs/lib/models/Fields.php
@@ -1,16 +1,13 @@
 <?php
 
-class Fields extends \Phalcon\Mvc\Model
-{
+class Fields extends \Phalcon\Mvc\Model {
 	public static $publicFields = ['id', 'name', 'formType', 'defaultValue', 'placeholder', 'helpText', 'dbFieldName', 'required'];
 
-    public function getSource()
-    {
-        return 'apacs_' . 'fields';
-    }
+	public function getSource() {
+		return 'apacs_' . 'fields';
+	}
 
-    public function initialize()
-    {
-    	$this->belongsTo('id', 'FieldsFieldgroup', 'field_id');
-    }
+	public function initialize() {
+		$this->belongsTo('id', 'EntitiesFields', 'field_id');
+	}
 }
