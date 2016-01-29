c315cd1419a03e2516b5abebb8bb75c17d89b952
Bo Henriksen <stumpdenmark@hotmail.com>
2016-01-29 18:08:04 +0100

    Updated Entities and Entries to support JSONSchema and new entity structure

--- apacs/lib/models/Entities.php
+++ apacs/lib/models/Entities.php
@@ -2,59 +2,136 @@
 
 use Phalcon\Mvc\Model\Query;
 
-class Entities extends \Phalcon\Mvc\Model
-{
-    public static $publicFields = ['id', 'required', 'countPerEntry', 'isMarkable','guiName', 'task_id'];
+class Entities extends \Phalcon\Mvc\Model {
+	public static $publicFields = ['id', 'required', 'countPerEntry', 'isMarkable', 'guiName', 'task_id'];
 
-    public function getSource()
-    {
-        return 'apacs_' . 'entities';
-    }
+	public function getSource() {
+		return 'apacs_' . 'entities';
+	}
 
-    public function initialize()
-    {
-  	 	$this->hasMany('id', 'EntitiesFields', 'entity_id');
-    	$this->belongsTo('task_id', 'Task', 'id');
-    }
+	public function initialize() {
+		$this->hasMany('id', 'EntitiesFields', 'entity_id');
+		$this->belongsTo('task_id', 'Task', 'id');
+	}
 
-    public static function LoadEntitiesHierarchy($taskId)
-    {
-        $parentEntities = Entities::find(['conditions' => 'task_id = ' . $taskId . ' AND parent_id IS NULL']);
+	private function GetEntitiesFields($entityId) {
+		$query = new Query("SELECT Entities.id as entity_id, EntitiesFields.id as entity_field_id, Fields.name, Fields.type, Fields.validationRegularExpression, Fields.helpText, Fields.validationErrorMessage, Fields.defaultValue, Fields.required, Fields.foreignEntityName, Fields.foreignFieldName, Fields.dbFieldName FROM Entities LEFT JOIN EntitiesFields ON Entities.id = EntitiesFields.entity_id LEFT JOIN Fields ON EntitiesFields.field_id = Fields.id WHERE Entities.id = :entityId:", $this->getDI());
+		$fields = $query->execute(['entityId' => $entityId])->toArray();
+		return $fields;
+	}
 
-        //Loading entities with children
-        $entities = [];
-        $i = 0;
-        foreach($parentEntities as $entity)
-        {
-            $entities[$i] = Entities::GetEntityRecursively($entity);
-            $i++;
-        }
+	public function getEntityAsJSONSchema($entity) {
 
-        return $entities;
-    }
+		foreach ($entity['fields'] as $key => $field) {
+			//If the field has fields, it is an object or array, and is converted recursively
+			if (isset($field['fields'])) {
+				$entity['fields'][$key] = $this->getEntityAsJSONSchema($entity['fields'][$key]);
+			}
+		}
+		//Converting fields to assoc array
+		$entity['fields'] = $this->ConvertFieldsToAssocArray($entity['fields']);
+		//Converting the entity itself to JSONSchema object
+		$entity = $this->ConvertEntityToJSONSchemaObject($entity);
+		return $entity;
+	}
 
-    private static function GetEntityRecursively($ingoingEntity)
-    {
-        $entity = $ingoingEntity->toArray();
+	public function GetEntityAndFields($entityId) {
+		$entity = Entities::find(['conditions' => 'id =' . $entityId])[0];
+		$entityArr = [];
+		$entityArr = $entity->toArray();
+		$entityArr['fields'] = $this->GetEntitiesFields($entityId);
 
-        //Loading fields
-        $query = new Query('SELECT f.* FROM EntitiesFields as ef LEFT JOIN Fields as f ON ef.field_id = f.id WHERE ef.entity_id = ' . $ingoingEntity->id, \Phalcon\DI\FactoryDefault::getDefault());
-        $fields  = $query->execute()->toArray();
-        
-        //Adding the already known table name manually
-        for($i = 0; $i < count($fields); $i++)
-        {
-            $fields[$i]['dbTableName'] = $ingoingEntity->dbTableName;
-        }
-       
-        $entity['fields'] = $fields;
+		//Loading related entities by identifying fields of type object and array
+		foreach (array_filter($entityArr['fields'], function ($el) {return $el['type'] == 'object' || $el['type'] == 'array';}) as $key => $field) {
+			//var_dump($field);
+			if ($field['type'] == 'object') {
+				$entityArr['type'] = 'object';
+			} else {
+				$entityArr['type'] = 'array';
+			}
 
-        foreach(Entities::find(['conditions' => 'parent_id = ' . $ingoingEntity->id]) as $child){
-            $c = Entities::GetEntityRecursively($child);
-            if($c != NULL){
-                $entity['children'][] = $c;
-            }
-        }
-        return $entity;
-    }
+			$entityArr['fields'][$key] = $this->GetEntityAndFields($field['foreignEntityName']);
+		}
+
+		return $entityArr;
+	}
+
+	public function ConvertFieldsToAssocArray($fields) {
+		$keyArr = [];
+		foreach ($fields as $field) {
+			if (isset($field['dbFieldName'])) {
+				$keyArr[$field['dbFieldName']] = $field;
+			} else if (isset($field['dbTableName'])) {
+				$keyArr[$field['dbTableName']] = $field;
+			} else {
+				$keyArr[] = $field;
+			}
+		}
+
+		return $keyArr;
+	}
+
+	public function ConvertEntityToJSONSchemaObject($entity) {
+		foreach ($entity as $key => $property) {
+			switch ($key) {
+			case 'dbTableName':
+				$entity['title'] = $entity[$key];
+				break;
+			case 'name':
+				$entity['title'] = $entity[$key];
+				break;
+			case 'validationRegularExpression':
+				$entity['pattern'] = $entity[$key];
+				break;
+			}
+		}
+
+		if ($entity['countPerEntry'] == 1) {
+			$entity['type'] = 'object';
+			$entity['properties'] = $entity['fields'];
+		} else {
+			$entity['type'] = 'array';
+			$entity['items'] = $entity['fields'];
+		}
+		unset($entity['fields']);
+
+		return $entity;
+	}
+
+	public static function LoadEntitiesHierarchy($taskId) {
+		$parentEntities = Entities::find(['conditions' => 'task_id = ' . $taskId . ' AND parent_id IS NULL']);
+
+		//Loading entities with children
+		$entities = [];
+		$i = 0;
+		foreach ($parentEntities as $entity) {
+			$entities[$i] = Entities::GetEntityRecursively($entity);
+			$i++;
+		}
+
+		return $entities;
+	}
+
+	private static function GetEntityRecursively($ingoingEntity) {
+		$entity = $ingoingEntity->toArray();
+
+		//Loading fields
+		$query = new Query('SELECT f.* FROM EntitiesFields as ef LEFT JOIN Fields as f ON ef.field_id = f.id WHERE ef.entity_id = ' . $ingoingEntity->id, \Phalcon\DI\FactoryDefault::getDefault());
+		$fields = $query->execute()->toArray();
+
+		//Adding the already known table name manually
+		for ($i = 0; $i < count($fields); $i++) {
+			$fields[$i]['dbTableName'] = $ingoingEntity->dbTableName;
+		}
+
+		$entity['fields'] = $fields;
+
+		foreach (Entities::find(['conditions' => 'parent_id = ' . $ingoingEntity->id]) as $child) {
+			$c = Entities::GetEntityRecursively($child);
+			if ($c != NULL) {
+				$entity['children'][] = $c;
+			}
+		}
+		return $entity;
+	}
 }
--- apacs/lib/models/Entries.php
+++ apacs/lib/models/Entries.php
@@ -1,332 +1,262 @@
 <?php
-use Phalcon\Mvc\Model\Query;
 
-class Entries extends \Phalcon\Mvc\Model
-{
+class Entries extends \Phalcon\Mvc\Model {
 
 	protected $id;
 	protected $pagesId;
 	protected $tasksId;
 	protected $collectionId;
 
-    private $_metaEntities;
+	private $_metaEntities;
 
-    public function getSource()
-    {
-        return 'apacs_' . 'entries';
-    }
+	public function getSource() {
+		return 'apacs_' . 'entries';
+	}
 
-    public function initialize()
-    {
-        $this->hasMany('id', 'Errors', 'entry_id');
-        $this->belongsTo('page_id', 'Pages', 'id');
-        $this->belongsTo('task_id', 'Tasks', 'id');
-    }
+	public function initialize() {
+		$this->hasMany('id', 'Errors', 'entry_id');
+		$this->belongsTo('page_id', 'Pages', 'id');
+		$this->belongsTo('task_id', 'Tasks', 'id');
+	}
 
-    public static function SaveInSolr($data)
-    {
-        $config = [ 
-            'endpoint' => 
-                [ 'localhost' => 
-                    [ 'host' => '54.194.233.62', 'hostname' => '54.194.233.62', 'port' => 80, 'login' => '', 'path' => '/solr/apacs_core'] 
-                ]
-            ];
+	public static function SaveInSolr($data) {
+		$config = [
+			'endpoint' =>
+			['localhost' =>
+				['host' => '54.194.233.62', 'hostname' => '54.194.233.62', 'port' => 80, 'login' => '', 'path' => '/solr/apacs_core'],
+			],
+		];
 
+		// create a client instance
+		$client = new Solarium\Client($config);
 
-        // create a client instance
-        $client = new Solarium\Client($config);
+		// get a select query instance
+		//   $query = $client->createQuery($client::QUERY_SELECT);
 
+		// $resultset = $client->execute($query);
 
-        // get a select query instance
-     //   $query = $client->createQuery($client::QUERY_SELECT);
+		$update = $client->createUpdate();
+		$doc1 = $update->createDocument();
+		$doc1->id = rand(0, 10000000);
+		$doc1->collection_id = 1;
+		$doc1->page_id = 1;
+		$doc1->post_id = 1;
+		$doc1->task_id = 1;
+		$doc1->entry_id = 1;
+		$doc1->firstnames = 'testdoc-1';
+		$doc1->lastname = "364";
+		$doc1->birthdate = "1972-05-20T17:33:18Z";
+		$doc1->birthplace = "København";
 
-       // $resultset = $client->execute($query);
+		/*     $childDoc = $update->createDocument();
+			        //$childDoc = [];
+			        $childDoc->id = rand(0,1000000);
+			        $childDoc->address1_s ='Jens Nielsens Allé 1, st. tv., 2100 København Ø';
+			        $childDoc->street = 'Jens Nielsens Allé';
+			        $childDoc->parent_id = $doc1->id;
+			        $childDoc->collection_id = 1;
+			        $childDoc->page_id = 1;
+			        $childDoc->post_id = 1;
+			        $childDoc->task_id = 1;
+		*/
+		$doc1->address = ['Jens Nielsens Allé 1, st. tv., 2100 København Ø', 'Rådhuspladsen 13, midtfor, 1599 København K'];
+		$doc1->area = ['Storstrøms Amt', 'Københavns Amt'];
+		$doc1->sogn = ['Testsogn'];
+		/*
+			        $childDoc1 = $update->createDocument();
+			       // $childDoc1 = [];
+			        $childDoc1->id = rand(0,1000000);
+			        $childDoc1->address2_s ='Rådhuspladsen 13, midtfor, 1599 København K';
+			        $childDoc1->street = 'Rådhuspladsen';
+			        $childDoc1->parent_id = $doc1->id;
+			        $childDoc1->collection_id = 1;
+			        $childDoc1->page_id = 1;
+			        $childDoc1->post_id = 1;
+			        $childDoc1->task_id = 1;
+			        $childDoc1->entry_id = 1;
+		*/
+		//      $doc1->address_concat[] = $childDoc1->address;
 
-        $update = $client->createUpdate();
-        $doc1 = $update->createDocument(); 
-        $doc1->id = rand(0,10000000); 
-        $doc1->collection_id = 1;
-        $doc1->page_id = 1;
-        $doc1->post_id = 1;
-        $doc1->task_id = 1;
-        $doc1->entry_id = 1;
-        $doc1->firstnames = 'testdoc-1'; 
-        $doc1->lastname = "364";
-        $doc1->birthdate = "1972-05-20T17:33:18Z";
-        $doc1->birthplace = "København";
-                
-   /*     $childDoc = $update->createDocument();
-        //$childDoc = [];
-        $childDoc->id = rand(0,1000000);
-        $childDoc->address1_s ='Jens Nielsens Allé 1, st. tv., 2100 København Ø';
-        $childDoc->street = 'Jens Nielsens Allé';
-        $childDoc->parent_id = $doc1->id;
-        $childDoc->collection_id = 1;
-        $childDoc->page_id = 1;
-        $childDoc->post_id = 1;
-        $childDoc->task_id = 1;
-        $childDoc->entry_id = 1;*/
-        $doc1->address = ['Jens Nielsens Allé 1, st. tv., 2100 København Ø', 'Rådhuspladsen 13, midtfor, 1599 København K'];
-        $doc1->area = ['Storstrøms Amt', 'Københavns Amt'];
-        $doc1->sogn = ['Testsogn'];
-  /*
-        $childDoc1 = $update->createDocument();
-       // $childDoc1 = [];
-        $childDoc1->id = rand(0,1000000);
-        $childDoc1->address2_s ='Rådhuspladsen 13, midtfor, 1599 København K';
-        $childDoc1->street = 'Rådhuspladsen';        
-        $childDoc1->parent_id = $doc1->id;
-        $childDoc1->collection_id = 1;
-        $childDoc1->page_id = 1;
-        $childDoc1->post_id = 1;
-        $childDoc1->task_id = 1;
-        $childDoc1->entry_id = 1;
-*/
-  //      $doc1->address_concat[] = $childDoc1->address;
+		$doc2 = $update->createDocument();
+		$doc2->id = rand(0, 10000000);
+		$doc2->collection_id = 1;
+		$doc2->page_id = 1;
+		$doc2->post_id = 1;
+		$doc2->task_id = 1;
+		$doc2->entry_id = 1;
+		$doc2->firstnames = 'testdoc-2';
+		$doc2->lastname = "340";
+		$doc2->birthdate = "1972-05-20T17:33:18Z";
+		$doc2->birthplace = "København";
+		$doc2->address = ['Hans Knudsens Plads 23, 2100 København Ø'];
 
+		$result = $update->addDocuments([$doc1, $doc2]);
+		$update->addCommit();
+		$result = $client->update($update);
+		//echo  $result->getStatus();
 
-        $doc2 = $update->createDocument(); 
-        $doc2->id = rand(0,10000000); 
-        $doc2->collection_id = 1;
-        $doc2->page_id = 1;
-        $doc2->post_id = 1;
-        $doc2->task_id = 1;
-        $doc2->entry_id = 1;
-        $doc2->firstnames = 'testdoc-2';
-        $doc2->lastname = "340";
-        $doc2->birthdate = "1972-05-20T17:33:18Z";
-        $doc2->birthplace = "København";
-        $doc2->address = ['Hans Knudsens Plads 23, 2100 København Ø'];
+		// get a select query instance
+		$query = $client->createQuery($client::QUERY_SELECT);
+		// this executes the query and returns the result
+		$resultset = $client->execute($query);
+		// display the total number of documents found by solr
+		//  echo 'NumFound: '.$resultset->getNumFound();
 
+		$this->response->setJsonContent($resultset->GetData());
+	}
 
-        $result = $update->addDocuments([$doc1, $doc2]);
-        $update->addCommit();
-        $result = $client->update($update);
-        //echo  $result->getStatus();
+	public static function SaveEntryRecursively($entityName, $entityFields, $values, $dbCon) {
+		$entityTableAndFields = Entities::find(['conditions' => 'foreignEntityName = ' . $entityName['dbTableName']])[0];
 
-        // get a select query instance
-        $query = $client->createQuery($client::QUERY_SELECT);
-        // this executes the query and returns the result
-        $resultset = $client->execute($query);
-        // display the total number of documents found by solr
-      //  echo 'NumFound: '.$resultset->getNumFound();
+		//Process:
+		//Save entities for fields of type object
+		//Save entity including ids from saved objects
+		//Save entities for fields of type array with id from saved entity
 
-        echo json_encode($resultset->GetData());
+		//Saving related entities with relation type object
+		foreach (array_filter($entityFields, function ($el) {return $el['type'] == 'object';}) as $objectField) {
+			$tableAndFields = $this->EntityFactory->Get($objectField['foreignEntity']);
 
+			$newId = Entries::SaveEntryRecursively($objectField['foreignTableName'], $tableAndFields['fields'], $values[$tableAndFields['dbTableName']], $dbCon);
 
-    }
+			$values[$tableAndFields['dbTaleName']] = $newId;
+		}
 
-    public static function SaveEntryRecursively($dbCon, $entity, $values, $parentId = NULL)
-    {
-        $genericEntry = new GenericEntry($entity['dbTableName'], $entity['fields'], $dbCon);
-        
-        //Setting parent ids
-        //We have several child elements
-        if(array_keys($values) == range(0, count($values) - 1))
-        {
-            for($i = 0; $i< count($values); $i++)
-            {
-                $values[$i]['parent_id'] = $parentId;
-            }
-        }
-        else{
-            $values['parent_id'] = $parentId;
-            $newVal = [];
-            $newVal[0] = $values;
-            $values = $newVal;
-        }
+		//Saving the entity
+		$entry = new GenericEntry($entityTable, $entityFields, $dbCon);
+		$entry->Save($values);
+		$entryId = $entry->GetInsertId();
 
-        for($i = 0; $i < count($values); $i++)
-        {
-            $insertId = -1;
+		//Saving related entities with relation type array
+		foreach (array_filter($entityFields, function ($el) {return $el['type'] == 'array';}) as $arrayField) {
+			$tableAndFields = $this->EntityFactory->Get($arrayField['foreignEntity']);
+			$values[$tableAndFields['dbTableName']]['foreignFieldName'] = $entryId;
 
-            //If entity is required to be unique
-            //we test if it exists
-            if($entity['unique'] == 1){
-                $result = $genericEntry->FindByValues($values[$i]);
-                if(count($result) > 0){
-                    $insertId = $result[0]['id'];
-                }
-            }
-            
-            if($insertId == -1){
-                if(!$genericEntry->Save($values[$i]))
-                    throw new Exception("Could not save!");
+			Entries::SaveEntryRecursively($arrayField['foreignTableName'], $tableAndFields['fields'], $values[$tableAndFields['dbTableName']], $dbCon);
+		}
 
-                $insertId = $genericEntry->GetInsertId();
-            }
+		return $entryId;
+	}
 
-            //Saving many to many relations here by adding a virtual entity and using the two ids
-            //(id and parent id)
-            if($entity['manyToManyTable'] !== NULL)
-            {
-                $middleEntity = [];
-                $middleEntity['tablename'] = $entity['manyToManyTable'];
+	public static function ValidateJSONData($schema, $data) {
+		var_dump($schema);
+		// Validate
+		$validator = new JsonSchema\Validator();
+		$validator->check($data, $schema);
 
-                //Creating a many to many entry, by using an artificial code table
-                $middleEntity['fields'][] = [
-                    'dbFieldName' => $entity['manyToManyForeignField'], 
-                    'codeTable' => NULL, 
-                    'codeField' => NULL, 
-                    'codeAllowNewValue' => 0
-                ];
+		$messages = [];
+		if (!$validator->isValid()) {
+			foreach ($validator->getErrors() as $error) {
+				$messages[] = sprintf("[%s] %s\n", $error['property'], $error['message']);
+			}
+		}
 
-                $middleEntity['fields'][] = [
-                    'dbFieldName' => $entity['manyToManyParentField'],
-                    'codeTable' => NULL, 
-                    'codeField' => NULL, 
-                    'codeAllowNewValue' => 0
-                ];
+		return $messages;
+	}
 
-                $middleData[$entity['manyToManyParentField']] = $values[$i]['parent_id'];
-                $middleData[$entity['manyToManyForeignField']] = $insertId;
+	/**
+	 * Validates an array of entities (POST data from users)
+	 * @param Array $entities An array of entities with data
+	 * @return bool Returns true is all fields are valid, false if not
+	 */
+	public function ValidateEntry(Array $entities) {
+		//Array for metaEntities. Loaded as entities of specific metaEntity type is reached
+		$metaEntities = [];
+		$isValid = true;
 
-                $middleGE = new GenericEntry($middleEntity['tablename'], $middleEntity['fields'], $dbCon);
-                
-                if(!$middleGE->Save($middleData))
-                {
-                    throw new Exception("Could not save middle data!");
-                }
-            }
+		for ($j = 0; $j < count($entities); $j++) {
+			$entity = $entities[$j];
 
-            //Saving children entities
-            if(isset($entity['children']))
-            {
-                foreach($entity['children'] as $child)
-                {               
-                    if(isset($values[$i][$child['guiName']])){
-                        Entries::SaveEntryRecursively($dbCon, $child, $values[$i][$child['guiName']], $insertId);
-                    }
-                }
-            }
-        }
-    }   
+			//Get metaEntity
+			$this->loadMetaEntity($entity['entity_id']);
 
-    public static function ValidateJSONData($schema, $data)
-    {
-        var_dump($schema);
-        // Validate
-        $validator = new JsonSchema\Validator();
-        $validator->check($data, $schema);
+			$ge = new GenericEntry($this->metaEntities[$entity['entity_id']], $entity['fields'], $this->getDI());
 
-        $messages = [];
-        if (!$validator->isValid()){
-            foreach ($validator->getErrors() as $error) {
-                $messages[] = sprintf("[%s] %s\n", $error['property'], $error['message']);
-            }
-        }
+			if (!$ge->ValidateData()) {
+				$isValid = false;
+			}
 
-        return $messages;
-    }
+			$entities[$j] = $entity;
+		}
 
-    /**
-     * Validates an array of entities (POST data from users)
-     * @param Array $entities An array of entities with data
-     * @return bool Returns true is all fields are valid, false if not
-     */
-    public function ValidateEntry(Array $entities)
-    {
-        //Array for metaEntities. Loaded as entities of specific metaEntity type is reached
-        $metaEntities = [];
-        $isValid = true;
+		return $isValid;
+	}
 
-        for($j = 0; $j < count($entities); $j++)
-        {
-            $entity = $entities[$j];
+	public function UpdateEntry() {
 
-            //Get metaEntity
-            $this->loadMetaEntity($entity['entity_id']);
+	}
 
-            $ge = new GenericEntry($this->metaEntities[$entity['entity_id']], $entity['fields'], $this->getDI());
+	public function LoadEntitiesByPost($postId) {
+		$entries = Entries::find(['post_id' => $postId]);
 
-            if(!$ge->ValidateData()){
-                $isValid = false;
-            }
+		$entriesData = [];
 
-            $entities[$j] = $entity;
-        }
+		foreach ($entries as $entry) {
+			$metaEntity = $this->loadMetaEntity($entry->entity_id);
+			$ge = new GenericEntry($metaEntity, [], $this->getDI());
 
-        return $isValid;
-    }
+			$entriesData[] = $ge->Load($postId);
+		}
 
-    public function UpdateEntry()
-    {
+		$this->response->setJsonContent($entriesData);
+	}
 
-    }
+	/*public function UpdateEntryData($id)
+		    {
+		        //This is the heavy one! What we want here is this:
+		        //Get all metaEntities
+		        //Get all metaFields
+		        //Get concrete field data from entities
+		        //Map it all together in this:
+		        /*
 
-    public function LoadEntitiesByPost($postId)
-    {
-        $entries = Entries::find(['post_id' => $postId]);
+		        {
+		            entity_groups:[
+		                {
+		                    name: "Hovedperson",
+		                    entities: [
+		                        {
+		                            id: 232,
+		                            fields: [
+		                                {
+		                                    id: 3525
+		                                    fieldname: fornavn,
+		                                    value: Hans,
+		                                    unreadable: false
+		                                }
+		                            ]
+		                        },
+		                        {
+		                            id: 252,
+		                            fields: [
+		                                {
+		                                    id: 32425,
+		                                    fieldname: fornavn,
+		                                    value: Jens,
+		                                    unreadable: false
+		                                }
+		                            ]
+		                        }
+		                    ]
+		                },
+		                {
+		                    name: "Ægtefæller",
+		                    entities: [
+		                        {
+		                            id: 523,
+		                            fields: [
+		                                {
+		                                    id: 2423,
+		                                    fieldname: fornavn,
+		                                    value: Jensine,
+		                                    unreadable: true
+		                                }
+		                            ]
+		                        }
+		                    ]
+		                }
+		            ]
+		        }
 
-        $entriesData = [];
-
-        foreach($entries as $entry)
-        {
-            $metaEntity = $this->loadMetaEntity($entry->entity_id);
-            $ge = new GenericEntry($metaEntity, [], $this->getDI());
-
-            $entriesData[] = $ge->Load($postId);
-        }
-
-        $this->response->setJsonContent($entriesData);
-    }
-
-    /*public function UpdateEntryData($id)
-    {
-        //This is the heavy one! What we want here is this:
-        //Get all metaEntities
-        //Get all metaFields
-        //Get concrete field data from entities
-        //Map it all together in this:
-        /*
-        
-        {
-            entity_groups:[
-                {
-                    name: "Hovedperson",
-                    entities: [
-                        {
-                            id: 232,
-                            fields: [
-                                {
-                                    id: 3525
-                                    fieldname: fornavn,
-                                    value: Hans,
-                                    unreadable: false
-                                }
-                            ]
-                        },
-                        {
-                            id: 252,
-                            fields: [
-                                {
-                                    id: 32425,
-                                    fieldname: fornavn,
-                                    value: Jens,
-                                    unreadable: false
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    name: "Ægtefæller",
-                    entities: [
-                        {
-                            id: 523,
-                            fields: [
-                                {
-                                    id: 2423,
-                                    fieldname: fornavn,
-                                    value: Jensine,
-                                    unreadable: true
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]            
-        }
-        
-        */  
+	*/
 }
