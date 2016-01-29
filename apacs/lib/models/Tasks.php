1a2ca853630a2e70a92467fd2206d08b4082b465
Bo Henriksen <stumpdenmark@hotmail.com>
2016-01-29 18:08:56 +0100

    Prettified tasks

--- apacs/lib/models/Tasks.php
+++ apacs/lib/models/Tasks.php
@@ -2,63 +2,58 @@
 
 use Phalcon\Mvc\Model\Query;
 
-class Tasks extends \Phalcon\Mvc\Model
-{
-    public function getSource()
-    {
-        return 'apacs_' . 'tasks';
-    }
+class Tasks extends \Phalcon\Mvc\Model {
+	public function getSource() {
+		return 'apacs_' . 'tasks';
+	}
 
-    public function initialize()
-    {
-        $this->hasMany('id', 'TasksUnits', 'task_id');
-        $this->hasMany('id', 'Entries', 'task_id');
-        $this->hasMany('id', 'TasksPages', 'task_id');
-        $this->hasMany('id', 'Entities', 'task_id');
-        $this->belongsTo('collection_id', 'Collections', 'id');
-    }
+	public function initialize() {
+		$this->hasMany('id', 'TasksUnits', 'task_id');
+		$this->hasMany('id', 'Entries', 'task_id');
+		$this->hasMany('id', 'TasksPages', 'task_id');
+		$this->hasMany('id', 'Entities', 'task_id');
+		$this->belongsTo('collection_id', 'Collections', 'id');
+	}
 
-    public function GetFieldsSchema($entityId)
-    {
-        $query = new Query("SELECT Entities.parent_id, Entities.id as entity_id, EntitiesFields.id as entity_field_id, Fields.name as title, Fields.formType as type, Fields.validationRegularExpression as pattern, Fields.helpText as description, Fields.validationErrorMessage as validationMessage, Fields.defaultValue as default, Fields.required FROM Entities LEFT JOIN EntitiesFields ON Entities.id = EntitiesFields.entity_id LEFT JOIN Fields ON EntitiesFields.field_id = Fields.id WHERE Entities.id = :entityId:", $this->getDI());
-        $fields = $query->execute(['entityId' => $entityId])->toArray();
-        
-        usort($fields, function($a, $b) {
-            if($a['entity_id'] == $b['entity_id'])
-                return 0;
+	public function GetFieldsSchema($entityId) {
+		$query = new Query("SELECT Entities.id as entity_id, EntitiesFields.id as entity_field_id, Fields.name as title, Fields.type as type, Fields.validationRegularExpression as pattern, Fields.helpText as description, Fields.validationErrorMessage as validationMessage, Fields.defaultValue as default, Fields.required FROM Entities LEFT JOIN EntitiesFields ON Entities.id = EntitiesFields.entity_id LEFT JOIN Fields ON EntitiesFields.field_id = Fields.id WHERE Entities.id = :entityId:", $this->getDI());
+		$fields = $query->execute(['entityId' => $entityId])->toArray();
 
-            return ($a['entity_id'] < $b['entity_id']) ? -1 : 1;
-        });
+		usort($fields, function ($a, $b) {
+			if ($a['entity_id'] == $b['entity_id']) {
+				return 0;
+			}
 
-        $return = [];
-        $return['properties'] = [];
-        $return['required'] = [];
+			return ($a['entity_id'] < $b['entity_id']) ? -1 : 1;
+		});
 
+		$return = [];
+		$return['properties'] = [];
+		$return['required'] = [];
 
-        foreach($fields as $field)
-        {
-            if($field['required'] == 1){
-                $return['required'][] = $field['entity_field_id'];
-            }
+		foreach ($fields as $field) {
+			if ($field['required'] == 1) {
+				$return['required'][] = $field['entity_field_id'];
+			}
 
-            unset($field['required']);
-        //    unset($field['entity_id']);
-          //  unset($field['parent_id']);
-            if($field['type'] == 'typeahead'){
-                $field['type'] = 'string';
-                $field['format'] = 'typeahead';
-            }
-            $return['properties'][$field['entity_field_id']] = $field;
-        }
+			unset($field['required']);
+			//    unset($field['entity_id']);
+			//  unset($field['parent_id']);
+			if ($field['type'] == 'typeahead') {
+				$field['type'] = 'string';
+				$field['format'] = 'typeahead';
+			}
+			$return['properties'][$field['entity_field_id']] = $field;
+		}
 
-        return $return;
-    }
+		return $return;
+	}
 
-    private function compareArray($a, $b)
-    {
-        if($a['entity_id'] == $b['entity_id'])
-            return 0;
+	private function compareArray($a, $b) {
+		if ($a['entity_id'] == $b['entity_id']) {
+			return 0;
+		}
 
-        return ($a['entity_id'] < $b['entity_id']) ? -1 : 1;
-    }
+		return ($a['entity_id'] < $b['entity_id']) ? -1 : 1;
+	}
 }
