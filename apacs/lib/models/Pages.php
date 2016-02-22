277fa6e804da79a62a0a77a2a74cb32742867cfb
Bo Henriksen <stumpdenmark@hotmail.com>
2016-02-23 00:31:21 +0100

    Pages model updated

--- apacs/lib/models/Pages.php
+++ apacs/lib/models/Pages.php
@@ -1,86 +1,91 @@
 <?php
 
-class Pages extends \Phalcon\Mvc\Model
-{
+class Pages extends \Phalcon\Mvc\Model {
 
 	protected $id;
 	protected $unitsId;
 	protected $collectionId;
 
-    private $status = [];
-    static $publicFields = ['id','collection_id','unit_id'];
+	private $status = [];
+	static $publicFields = ['id', 'collection_id', 'unit_id'];
 
-    const OPERATION_TYPE_CREATE = 'create';
-    const OPERATION_TYPE_UPDATE = 'update';
+	const OPERATION_TYPE_CREATE = 'create';
+	const OPERATION_TYPE_UPDATE = 'update';
 
-    public function getSource()
-    {
-        return 'apacs_' . 'pages';
-    }
+	public function getSource() {
+		return 'apacs_' . 'pages';
+	}
 
-    public function initialize()
-    {
-        $this->hasMany('id', 'Entries', 'page_id');
-        $this->hasMany('id', 'TasksPages', 'page_id');
-        $this->belongsTo('unit_id', 'Units', 'id');
-    }
+	public function initialize() {
+		$this->hasMany('id', 'Entries', 'page_id');
+		$this->hasMany('id', 'TasksPages', 'page_id');
+		$this->belongsTo('unit_id', 'Units', 'id');
+	}
 
-    private function getImportCreateSQL()
-    {
-        return 'INSERT INTO ' . $this->getSource() . ' (concrete_page_id, collection_id, concrete_unit_id, tablename, image_url) SELECT :id, :collectionId, :unitId, ":table", :imageUrl FROM :table :conditions';
-    }
+	public function GetLocalPathToConcreteImage() {
+		//Settings for ORM db access
+		ORM::configure('mysql:host=' . $this->getDI()->get('config')['host'] . ';dbname=' . $this->getDI()->get('config')['dbname']);
+		ORM::configure('username', $this->getDI()->get('config')['username']);
+		ORM::configure('password', $this->getDI()->get('config')['password']);
+		ORM::configure('charset', $this->getDI()->get('config')['charset']);
+		//ORM::configure('logging', true);
+		//echo ORM::get_last_query();
 
-    private function getImportUpdateSQL()
-    {
-        return 'UPDATE ' . $this->getSource() . ' LEFT JOIN :table ON ' . $this->getSource() . '.concrete_page_id = :table.:id SET tablename = ":table", image_url = :imageUrl :conditions';
-    }
+		$crud = new CRUD\CRUD();
 
-    public function Import($type, $collectionId, $idField, $unitIdField, $table, $image_url_field, $conditions = NULL)
-    {
-        if($type == self::OPERATION_TYPE_CREATE && $this->dataAlreadyImported('apacs_pages', $collectionId)){
-            $this->status = ['error' => 'pages are already imported (collection and tablename already exists']; 
-            return false;
-        }
+		$joins = ORM::for_table($this->getUnits()->getCollections()->concreteImagesTableName);
+		return $_SERVER['DOCUMENT_ROOT'] . '/../collections/' . $joins->select('relative_filename_converted')->where('id', $this->former_id)->find_one()['relative_filename_converted'];
+	}
 
-        $sql = ($type == self::OPERATION_TYPE_UPDATE ? $this->getImportUpdateSQL() : $this->getImportCreateSQL());
+	private function getImportCreateSQL() {
+		return 'INSERT INTO ' . $this->getSource() . ' (concrete_page_id, collection_id, concrete_unit_id, tablename, image_url) SELECT :id, :collectionId, :unitId, ":table", :imageUrl FROM :table :conditions';
+	}
 
-        $sql = str_replace(':collectionId', $collectionId, $sql);
-        $sql = str_replace(':id', $idField, $sql);
-        $sql = str_replace(':unitId', $unitIdField, $sql);
-        $sql = str_replace(':table', $table, $sql);
-        $sql = str_replace(':imageUrl', $image_url_field, $sql);
-        $sql = str_replace(':conditions', $conditions == NULL ? '' : 'WHERE ' . $conditions , $sql);
+	private function getImportUpdateSQL() {
+		return 'UPDATE ' . $this->getSource() . ' LEFT JOIN :table ON ' . $this->getSource() . '.concrete_page_id = :table.:id SET tablename = ":table", image_url = :imageUrl :conditions';
+	}
 
-        return $this->runQueryGetStatus($sql);
-    }
+	public function Import($type, $collectionId, $idField, $unitIdField, $table, $image_url_field, $conditions = NULL) {
+		if ($type == self::OPERATION_TYPE_CREATE && $this->dataAlreadyImported('apacs_pages', $collectionId)) {
+			$this->status = ['error' => 'pages are already imported (collection and tablename already exists'];
+			return false;
+		}
 
-    private function runQueryGetStatus($query)
-    {
-        $connection = $this->getDI()->get('database');
-        $success = $connection->execute($query);
+		$sql = ($type == self::OPERATION_TYPE_UPDATE ? $this->getImportUpdateSQL() : $this->getImportCreateSQL());
 
-        if($success){
-            $this->status = ["affected_rows" => $connection->affectedRows()];
-        }
-        else{
-            $this->status = ["status" => "could not execute query", "error_message" => $connection->getErrorInfo()];
-        }
+		$sql = str_replace(':collectionId', $collectionId, $sql);
+		$sql = str_replace(':id', $idField, $sql);
+		$sql = str_replace(':unitId', $unitIdField, $sql);
+		$sql = str_replace(':table', $table, $sql);
+		$sql = str_replace(':imageUrl', $image_url_field, $sql);
+		$sql = str_replace(':conditions', $conditions == NULL ? '' : 'WHERE ' . $conditions, $sql);
 
-        return $success;
-    }
+		return $this->runQueryGetStatus($sql);
+	}
 
-    private function dataAlreadyImported($type, $collectionId)
-    {
-        $sql = 'SELECT * FROM ' . $type . ' WHERE collection_id = \'' . $collectionId . '\' LIMIT 1';
-        $resultSet = $this->getDI()->get('database')->query($sql);
-        $resultSet->setFetchMode(Phalcon\Db::FETCH_ASSOC);
-        $results = $resultSet->fetchAll();
+	private function runQueryGetStatus($query) {
+		$connection = $this->getDI()->get('database');
+		$success = $connection->execute($query);
 
-        return count($results) > 0;
-    }    
+		if ($success) {
+			$this->status = ["affected_rows" => $connection->affectedRows()];
+		} else {
+			$this->status = ["status" => "could not execute query", "error_message" => $connection->getErrorInfo()];
+		}
 
-    public function GetStatus()
-    {
-        return $this->status;
-    }    
+		return $success;
+	}
+
+	private function dataAlreadyImported($type, $collectionId) {
+		$sql = 'SELECT * FROM ' . $type . ' WHERE collection_id = \'' . $collectionId . '\' LIMIT 1';
+		$resultSet = $this->getDI()->get('database')->query($sql);
+		$resultSet->setFetchMode(Phalcon\Db::FETCH_ASSOC);
+		$results = $resultSet->fetchAll();
+
+		return count($results) > 0;
+	}
+
+	public function GetStatus() {
+		return $this->status;
+	}
 }
