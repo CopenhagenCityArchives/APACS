4a2a5c870e5d8af70ac53c85264cf82a5e4c1c74
Bo Henriksen <stumpdenmark@hotmail.com>
2016-03-02 16:32:27 +0100

    TasksUnits supports finding units and tasks

--- apacs/app/models/TasksUnits.php
+++ apacs/app/models/TasksUnits.php
@@ -1,23 +1,25 @@
 <?php
 
-class TasksUnits extends \Phalcon\Mvc\Model
-{
+use Phalcon\Mvc\Model\Resultset\Simple as Resultset;
 
-	protected $id;
-	protected $pagesDone;
-    protected $isActive;
-    protected $entryLayout;
-    protected $unitId;
-    protected $taskId;
+class TasksUnits extends \Phalcon\Mvc\Model {
 
-    public function getSource()
-    {
-        return 'apacs_' . 'tasks_units';
-    }
+	public function getSource() {
+		return 'apacs_' . 'tasks_units';
+	}
 
-    public function initialize()
-    {
-        $this->hasMany('id', 'Units', 'unit_id');
-        $this->hasMany('id', 'Tasks', 'task_id');
-    }
+	public function initialize() {
+		$this->hasMany('id', 'Units', 'unit_id');
+		$this->hasMany('id', 'Tasks', 'task_id');
+	}
+
+	public static function FindUnitsAndTasks($conditions, $params = null) {
+		$sql = "SELECT * FROM apacs_units as Units LEFT JOIN apacs_tasks_units as TasksUnits ON Units.id = TasksUnits.units_id WHERE $conditions";
+
+		// Base model
+		$units = new TasksUnits();
+
+		// Execute the query
+		return new Resultset(null, $units, $units->getReadConnection()->query($sql, $params));
+	}
 }
