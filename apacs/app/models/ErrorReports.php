f9f0efe0f97a03a402a304d5f69ea351a45a5b8b
Bo Henriksen <stumpdenmark@hotmail.com>
2016-12-01 14:51:18 +0100

    Updated ErrorReports to support deleted field

--- apacs/app/models/ErrorReports.php
+++ apacs/app/models/ErrorReports.php
@@ -15,21 +15,21 @@ class ErrorReports extends \Phalcon\Mvc\Model {
 	}
 
 	public function beforeSave() {
-		$this->superUserTime = date('Y-m-d H:i:s', strtotime("+1 week"));
+		$this->super_user_time = date('Y-m-d H:i:s', strtotime("+1 week"));
 		$this->last_update = date('Y-m-d H:i:s');
 	}
 
-	public function GetWithUsers($conditions) {
-		$query = new Query('SELECT Errors.*, Users.* FROM Errors LEFT JOIN Errors.user_id = Users.id LEFT JOIN Users Errors.reporting_user_id = Users.id WHERE ' . $conditions, $this->getDI());
+	/*public function GetWithUsers($conditions) {
+		$query = new Query('SELECT Errors.*, Users.* FROM Errors LEFT JOIN Errors.user_id = Users.id LEFT JOIN Users Errors.reporting_user_id = Users.id WHERE Errors.deleted = 0 AND ' . $conditions, $this->getDI());
 		return $query->execute(['taskId' => $this->tasks_id, 'stepsId' => $this->id]);
-	}
+	}*/
 
 	public static function FindByRawSql($conditions = null, $params = null) {
 		// A raw SQL statement
-		$sql = "SELECT apacs_errorreports.*, apacs_users.username, CONCAT(apacs_collections.name, ' ', apacs_units.description) as unit_description, apacs_fields.formName as field_formName FROM apacs_errorreports LEFT JOIN apacs_users ON apacs_errorreports.users_id = apacs_users.id LEFT JOIN apacs_posts ON apacs_errorreports.posts_id = apacs_posts.id LEFT JOIN apacs_pages ON apacs_posts.pages_id = apacs_pages.id LEFT JOIN apacs_units ON apacs_pages.unit_id = apacs_units.id LEFT JOIN apacs_collections ON apacs_units.collections_id = apacs_collections.id LEFT JOIN apacs_entities ON apacs_entities.id = apacs_errorreports.entities_id LEFT JOIN apacs_fields ON apacs_fields.entities_id = apacs_errorreports.entities_id AND apacs_errorreports.field_name = apacs_fields.fieldName";
+		$sql = "SELECT apacs_errorreports.*, apacs_users.username, CONCAT(apacs_collections.name, ' ', apacs_units.description) as unit_description, apacs_fields.formName as field_formName FROM apacs_errorreports LEFT JOIN apacs_users ON apacs_errorreports.users_id = apacs_users.id LEFT JOIN apacs_posts ON apacs_errorreports.posts_id = apacs_posts.id LEFT JOIN apacs_pages ON apacs_posts.pages_id = apacs_pages.id LEFT JOIN apacs_units ON apacs_pages.unit_id = apacs_units.id LEFT JOIN apacs_collections ON apacs_units.collections_id = apacs_collections.id LEFT JOIN apacs_entities ON apacs_entities.id = apacs_errorreports.entities_id LEFT JOIN apacs_fields ON apacs_fields.entities_id = apacs_errorreports.entities_id AND apacs_errorreports.field_name = apacs_fields.fieldName WHERE apacs_errorreports.deleted = 0";
 
 		if (!is_null($conditions)) {
-			$sql = $sql . ' WHERE ' . $conditions;
+			$sql = $sql . ' AND ' . $conditions;
 		}
 
 		// Base model
