48037f80a1359fe2443ea2284fd6abc92bf97e64
Bo Henriksen <stumpdenmark@hotmail.com>
2017-04-04 10:36:11 +0200

    Display_errors moved to config

--- apacs/app/bootstrap.php
+++ apacs/app/bootstrap.php
@@ -1,9 +1,7 @@
 <?php
 
 use Phalcon\Mvc\Micro\Collection as MicroCollection;
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 $app = new Phalcon\Mvc\Micro();
 
 try {
