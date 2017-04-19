49a5cd6bc7f27c29c2020933e663631139d86d4c
Bo Henriksen <stumpdenmark@hotmail.com>
2017-04-19 11:08:26 +0200

    Fixed SystemException time condition

--- apacs/app/models/SystemExceptions.php
+++ apacs/app/models/SystemExceptions.php
@@ -10,9 +10,19 @@ class SystemExceptions extends \Phalcon\Mvc\Model {
 	}
 
 	public function getLastExceptionsByTypeAndHours($type, $hours) {
-		$phql = 'SELECT * FROM apacs_exceptions WHERE CURRENT_TIMESTAMP - INTERVAL :hours HOUR AND type = :type ORDER BY time DESC';
+		$phql = 'SELECT * FROM apacs_exceptions WHERE time > NOW() - INTERVAL :hours HOUR AND type = :type ORDER BY time DESC';
 		$resultSet = $this->getDI()->get('db')->query($phql, ['hours' => $hours, 'type' => $type]);
 		$resultSet->setFetchMode(Phalcon\Db::FETCH_ASSOC);
-		return $resultSet->fetchAll();
+		$result = $resultSet->fetchAll();
+
+		foreach($result as &$row){
+			$row['details'] = json_decode($row['details'], true);
+			if(isset($row['details']['rawPostData'])){
+				$row['details']['entryData'] = json_decode($row['details']['rawPostData']);
+				unset($row['details']['rawPostData']);
+			}
+		}
+
+		return $result;
 	}
 }
