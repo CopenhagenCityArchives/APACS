705c02e2463da2428bedc89e05ad723cae203e1b
Bo Henriksen <stumpdenmark@hotmail.com>
2017-05-05 14:02:17 +0200

    Generic collections now have pages id as id and collections_id in data query

--- apacs/app/library/ConfigurationLoader.php
+++ apacs/app/library/ConfigurationLoader.php
@@ -112,11 +112,11 @@ class ConfigurationLoader {
 
 		$conf['primary_table_name'] = 'apacs_pages';
 
-		$conf['objects_query'] = 'select apacs_pages.id, apacs_collections.name, apacs_units.id, apacs_units.level1_value, apacs_units.level2_value, apacs_units.level3_value, CONCAT(\'/getfile.php?fileId=\', apacs_pages.id) as imageURL, apacs_units.id as starbas_id
+		$conf['objects_query'] = 'select apacs_pages.id as id, apacs_collections.name, apacs_units.id as units_id, apacs_units.level1_value, apacs_units.level2_value, apacs_units.level3_value, CONCAT(\'/getfile.php?fileId=\', apacs_pages.id) as imageURL, apacs_units.id as starbas_id
             FROM apacs_pages
             LEFT JOIN apacs_units ON apacs_pages.unit_id = apacs_units.id
             LEFT JOIN apacs_collections ON apacs_units.collections_id = apacs_collections.id
-            WHERE :query';
+            WHERE apacs_collections.id = ' . $collectionId . ' AND :query';
 		//apacs_collections.is_public = 1 AND
 
 		$conf['levels_type'] = 'hierarchy';
