ea74ae0112d46985f8d1577756664afef69113d7
Bo Henriksen <stumpdenmark@hotmail.com>
2017-06-26 11:52:54 +0200

    Added freetext_store in searchconfig

--- apacs/app/models/Collections.php
+++ apacs/app/models/Collections.php
@@ -11,7 +11,7 @@ class Collections extends \Phalcon\Mvc\Model {
 	}
 
 	public function GetSearchConfig() {
-		$phql = 'SELECT Fields.id, Fields.formFieldType, Fields.datasources_id, Fields.includeInSolr, Fields.SOLRFacet, SOLRFieldName as solr_name, SOLRFacet as facetable, formName as name, Fields.includeInSOLR as searchable, SOLRResult as include_in_result, SOLRResult as sortable /*, CASE WHEN Entities.isPrimaryEntity = 1 OR SOLRResult = 1 THEN 1 ELSE 0 END as sortable*/ FROM apacs_fields Fields left join apacs_entities Entities ON Fields.entities_id = Entities.id LEFT JOIN apacs_tasks Tasks ON Entities.task_id = Tasks.id LEFT JOIN apacs_collections Collections ON Tasks.collection_id = Collections.id WHERE Fields.includeInSOLR = 1 AND Tasks.id = :id';
+		$phql = 'SELECT Fields.id, Fields.formFieldType, Fields.datasources_id, Fields.includeInSolr, Fields.SOLRFacet, SOLRFieldName as solr_name, SOLRFacet as facetable, formName as name, CASE WHEN Fields.includeInSOLR = 1 THEN 1 WHEN Fields.fieldName = "freetext_store" THEN 1 ELSE 0 END as searchable, SOLRResult as include_in_result, SOLRResult as sortable /*, CASE WHEN Entities.isPrimaryEntity = 1 OR SOLRResult = 1 THEN 1 ELSE 0 END as sortable*/ FROM apacs_fields Fields left join apacs_entities Entities ON Fields.entities_id = Entities.id LEFT JOIN apacs_tasks Tasks ON Entities.task_id = Tasks.id LEFT JOIN apacs_collections Collections ON Tasks.collection_id = Collections.id WHERE (Fields.includeInSOLR = 1 OR Fields.fieldName = "freetext_store") AND Tasks.id = :id';
 		$resultSet = $this->getDI()->get('db')->query($phql, ['id' => $this->id]);
 		$resultSet->setFetchMode(Phalcon\Db::FETCH_ASSOC);
 		return $resultSet->fetchAll();
