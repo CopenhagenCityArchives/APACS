a87ee9c0441822c9d80cb09fe1039d75b9fcfff0
Bo Henriksen <stumpdenmark@hotmail.com>
2018-03-07 09:55:50 +0100

    Added method for system exceptions loading by hour

--- apacs/app/models/SystemExceptions.php
+++ apacs/app/models/SystemExceptions.php
@@ -25,4 +25,21 @@ class SystemExceptions extends \Phalcon\Mvc\Model {
 
 		return $result;
 	}
+
+	public function getLastExceptionsByHours($hours) {
+		$phql = 'SELECT * FROM apacs_exceptions WHERE time > NOW() - INTERVAL :hours HOUR ORDER BY time DESC';
+		$resultSet = $this->getDI()->get('db')->query($phql, ['hours' => $hours]);
+		$resultSet->setFetchMode(Phalcon\Db::FETCH_ASSOC);
+		$result = $resultSet->fetchAll();
+
+		foreach($result as &$row){
+			$row['details'] = json_decode($row['details'], true);
+			if(isset($row['details']['rawPostData'])){
+				$row['details']['entryData'] = json_decode($row['details']['rawPostData']);
+				unset($row['details']['rawPostData']);
+			}
+		}
+
+		return $result;
+	}
 }
