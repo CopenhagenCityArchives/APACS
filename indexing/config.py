d40dbe3017f95debbf334d2b853c2b5550714047
Bo Henriksen <stumpdenmark@hotmail.com>
2018-04-13 16:08:02 +0200

    Index script now checks for environment flag

--- indexing/config.py
+++ indexing/config.py
@@ -1,47 +1,72 @@
 #! python3
 # -*- coding: utf-8 -*-
-
-Config = {
-    "debug" : False,
-    "cumulus" : {
-        "url": "https://neaonline.dk",
-        #"url": "https://cumulus",
-        "port": 8443,
-        "user": "CIP-erindringsbilleder",
-        "password": "***REMOVED***",
-        "catalog": "CIP-erindringsbilleder",
-        "layout": "erindringskatalog"
-    },
-    "kbhbilleder_db" : {
-        "host": "kbhbilleder-stats-cluster.cluster-cf2nstf005qs.eu-west-1.rds.amazonaws.com",
-        "port": 3306,
-        "user": "kbhbilleder-stat",
-        "password": "Ggc4Zpjm",
-        "database": "kbhbilleder-stats"
-    },
-    'polle_db' : {
-        "host": "138.201.87.24",
-        "port": 3306,
-        "user": "politietsregiste",
-        "password": "***REMOVED***",
-        "database": "politietsregisterblade"
-    },
-    "apacs_db" : {
-        "host": "148.251.122.164",
-        "port": 3306,
-        "user":"kbharkiv",
-        "password":"***REMOVED***",
-        "database": "kbharkiv"
-    },
-    "aws_sns" : {
-        "access_key_id": "***REMOVED***",
-        "secret_access_key": "***REMOVED***"
-    },
-    "solr": {
-        #"url":"https://aws.kbhkilder.dk/solr/apacs_core"
-        #"url":"http://ec2-34-240-1-32.eu-west-1.compute.amazonaws.com/solr/apacs_core"
-        #"url":"http://ec2-54-194-89-54.eu-west-1.compute.amazonaws.com/solr/apacs_core"
-        #"url": "http://solr:pass@solr:8983/solr/apacs_core"
-        "url": "http://solr:8983/solr/apacs_core"
+import os
+if os.getenv('PYTHON_ENV', 'DEV') == 'DEV':
+    print("Using development settings.")
+    Config = {
+        "debug" : True,
+        "cumulus" : {
+            #"url": "https://neaonline.dk",
+            "url": "https://cumulus",
+            "port": 8443,
+            "user": "CIP-erindringsbilleder",
+            "password": "***REMOVED***",
+            "catalog": "CIP-erindringsbilleder",
+            "layout": "erindringskatalog"
+        },
+        'polle_db' : {
+            "host": "138.201.87.24",
+            "port": 3306,
+            "user": "politietsregiste",
+            "password": "***REMOVED***",
+            "database": "politietsregisterblade"
+        },
+        "apacs_db" : {
+            "host": "148.251.122.164",
+            "port": 3306,
+            "user":"kbharkiv",
+            "password":"***REMOVED***",
+            "database": "kbharkiv"
+        },
+        "aws_sns" : {
+            "access_key_id": "***REMOVED***",
+            "secret_access_key": "***REMOVED***"
+        },
+        "solr": {
+            "url": "http://solr:8983/solr/apacs_core"
+        }
     }
-}
+else:
+    print("Using production settings!")
+    Config = {
+        "debug" : False,
+        "cumulus" : {
+            "url": "https://neaonline.dk",
+            "port": 443,
+            "user": "CIP-erindringsbilleder",
+            "password": "***REMOVED***",
+            "catalog": "CIP-erindringsbilleder",
+            "layout": "erindringskatalog"
+        },
+        'polle_db' : {
+            "host": "138.201.87.24",
+            "port": 3306,
+            "user": "politietsregiste",
+            "password": "***REMOVED***",
+            "database": "politietsregisterblade"
+        },
+        "apacs_db" : {
+            "host": "148.251.122.164",
+            "port": 3306,
+            "user":"kbharkiv",
+            "password":"***REMOVED***",
+            "database": "kbharkiv"
+        },
+        "aws_sns" : {
+            "access_key_id": "***REMOVED***",
+            "secret_access_key": "***REMOVED***"
+        },
+        "solr": {
+            "url": "http://solr:8983/solr/apacs_core"
+        }
+    }
