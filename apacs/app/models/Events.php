33b9058b5ddc2ddda8cb90ca471dbd6a5facdb34
Bo Henriksen <stumpdenmark@hotmail.com>
2018-05-16 10:49:20 +0200

    User events are now sorted by unit description and not id

--- apacs/app/models/Events.php
+++ apacs/app/models/Events.php
@@ -41,7 +41,7 @@ class Events extends \Phalcon\Mvc\Model {
 									LEFT JOIN apacs_pages as Pages on Events.pages_id = Pages.id
 									WHERE Events.users_id =  ' . $userId . ' group by unit_id) SUBQ
 			ON SUBQ.unit_id = Units.id AND SUBQ.time = timestamp
-			WHERE Events.users_id = ' . $userId . ' AND (event_type = \'' . self::TypeCreate . '\' OR event_type = \'' . self::TypeEdit . '\' OR event_type = \'' . self::TypeCreateUpdatePost . '\') order by Units.id';
+			WHERE Events.users_id = ' . $userId . ' AND (event_type = \'' . self::TypeCreate . '\' OR event_type = \'' . self::TypeEdit . '\' OR event_type = \'' . self::TypeCreateUpdatePost . '\') order by Units.description';
 
 		// Base model
 		$events = new Events();
