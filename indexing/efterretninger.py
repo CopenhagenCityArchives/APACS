0f71164589f8423d300f986c7e665c4998be9044
Jens Feodor Nielsen <jensfeodor@gmail.com>
2018-08-14 13:06:49 +0200

    Catch date errors

--- indexing/efterretninger.py
+++ indexing/efterretninger.py
@@ -54,39 +54,42 @@ if __name__ == "__main__":
 
 	writeflush("Creating Solr documents... ")
 	for i, efterretning in enumerate(cip.searchall("erindringskatalog", view="erindringskatalog", querystring="Samlingsnavn == 'Politiets Efterretninger' && Offentlig == true", chunk=50)):
-		writeflush("\rCreating Solr documents... %d" % (i+1))
-		# if(efterretning['Transkriberet'] == 1):
-		# 	query = 'ID == %s' % efterretning['Related Sub Assets']
-		# 	print(cip.search(catalog="erindringskatalog", querystring=query))
-		# 	sys.exit(1)
+		try:
+			writeflush("\rCreating Solr documents... %d" % (i+1))
+			# if(efterretning['Transkriberet'] == 1):
+			# 	query = 'ID == %s' % efterretning['Related Sub Assets']
+			# 	print(cip.search(catalog="erindringskatalog", querystring=query))
+			# 	sys.exit(1)
 
-		jsonObj = {}
-		jsonObj['id'] = "%d-%d" % (COLLECTION_ID, efterretning['ID'])
-		jsonObj['org_id'] = "%d" % efterretning['ID']
-		jsonObj['collection_id'] = COLLECTION_ID
-		jsonObj['number'] = efterretning.get("Nummer")
-		jsonObj['date'] = efterretning.get(u"Indsamlingsår").isoformat() if u"Indsamlingsår" in efterretning else None
-		jsonObj['fileName'] = efterretning.get("Record Name")
-
-		documents.append({
-			'id': "%d-%d" % (COLLECTION_ID, efterretning['ID']),
-			'task_id': -1,
-			'post_id': -1,
-			'entry_id': -1,
-			'user_id': -1,
-			'user_name': ' ',
-			'unit_id': -1,
-			'page_id': -1,
-			'jsonObj': json.dumps(jsonObj),
-			'collection_id': COLLECTION_ID,
-			'collection_info': 'Politiets Efterretninger',
-			'collected_year': efterretning.get(u"Indsamlingsår").year if u"Indsamlingsår" in efterretning else None,
-			'efterretning_number': efterretning.get("Nummer"),
-			'efterretning_date': efterretning.get(u"Indsamlingsår"),
-			'efterretning_fileName': efterretning.get("Record Name"),
-			'erindring_document_text': efterretning.get('Document Text')
-		})
-
+			jsonObj = {}
+			jsonObj['id'] = "%d-%d" % (COLLECTION_ID, efterretning['ID'])
+			jsonObj['org_id'] = "%d" % efterretning['ID']
+			jsonObj['collection_id'] = COLLECTION_ID
+			jsonObj['number'] = efterretning.get("Nummer")
+			jsonObj['date'] = efterretning.get(u"Indsamlingsår").isoformat() if u"Indsamlingsår" in efterretning else None
+			jsonObj['fileName'] = efterretning.get("Record Name")
+			
+			documents.append({
+				'id': "%d-%d" % (COLLECTION_ID, efterretning['ID']),
+				'task_id': -1,
+				'post_id': -1,
+				'entry_id': -1,
+				'user_id': -1,
+				'user_name': ' ',
+				'unit_id': -1,
+				'page_id': -1,
+				'jsonObj': json.dumps(jsonObj),
+				'collection_id': COLLECTION_ID,
+				'collection_info': 'Politiets Efterretninger',
+				'collected_year': efterretning.get(u"Indsamlingsår").year if u"Indsamlingsår" in efterretning else None,
+				'efterretning_number': efterretning.get("Nummer"),
+				'efterretning_date': efterretning.get(u"Indsamlingsår"),
+				'efterretning_fileName': efterretning.get("Record Name"),
+				'erindring_document_text': efterretning.get('Document Text')
+			})
+		except Exception as e:
+			msg = "Fejl under skabelse af Efterretning SOLR Dokument %d, sandsynligvis dato-formatering:\n\t%s\nIndhold af efterretning\n\t%s" % (i, repr(e), "\n\t".join(["%s : %s" % (k, v) for (k, v) in efterretning.items()]))
+			SNS_Notifier.error(msg)
 	print("")
 	chunksize = 100
 	index = 0
