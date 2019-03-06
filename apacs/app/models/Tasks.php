845c3c81466578412a8aab97ccae758e9f4f9892
Bo Henriksen <stumpdenmark@hotmail.com>
2019-03-06 11:25:33 +0100

    Removed now unused Entities, Fields and Steps models

--- apacs/app/models/Tasks.php
+++ apacs/app/models/Tasks.php
@@ -12,31 +12,4 @@ class Tasks extends \Phalcon\Mvc\Model {
 		$this->hasMany('id', 'Entities', 'task_id');
 		$this->belongsTo('collection_id', 'Collections', 'id');
 	}
-
-	public function GetTaskSchema() {
-		$mainEntity = Entities::findFirst(['conditions' => 'task_id = ' . $this->id . ' AND isPrimaryEntity = 1']);
-		$mainArr = $mainEntity->toArray();
-		$mainArr['title'] = $this->name;
-		$mainArr['description'] = $this->description;
-
-		$mainSchema = $mainEntity->ConvertToJSONSchemaObject();
-
-		$mainArr['required'] = $mainSchema['required'];
-		$mainArr['properties'] = $mainSchema['properties'];
-
-		$entities = Entities::find(['conditions' => 'task_id = ' . $this->id . ' AND isPrimaryEntity = 0']);
-
-		foreach ($entities as $entity) {
-			$mainArr['properties'][$entity->name] = $entity->ConvertToJSONSchemaObject();
-		}
-
-		$response = [];
-		$response['keyName'] = $mainEntity->name;
-		$response['schema']['type'] = 'object';
-		$response['schema']['properties'][$mainEntity->name] = $mainArr;
-		$steps = new Steps();
-		$response['steps'] = $steps->GetStepsAndFields($this->id, $mainEntity->name);
-
-		return $response;
-	}
 }
