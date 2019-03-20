cc070843746af997030aa7150844cb7b02f59191
Bo Henriksen <stumpdenmark@hotmail.com>
2019-03-20 11:48:02 +0100

    Updated deployment functionality

--- apacs/deployment/deploy.php
+++ apacs/deployment/deploy.php
@@ -7,18 +7,33 @@
     $user = getenv('FTP_USER');
     $password = getenv('FTP_PASSWORD');
 
+    $path = 'public_html/1508/public_beta';
+    $srcPath = dirname(__DIR__);
+
     $ftp = new \FtpClient\FtpClient();
+    echo 'connecting' . PHP_EOL;
     $ftp->connect($host, false, 21);
     $ftp->login($user, $password);
     $ftp->pasv(true);
-    var_dump($ftp->chdir('public_html'));
-    var_dump($ftp->nlist('.'));
+  //  var_dump($ftp->chdir('public_html'));
+  //  var_dump($ftp->nlist('.'));
   /*  var_dump($ftp->pwd());
     var_dump($ftp->chdir('public_html'));
     var_dump($ftp->pwd());
     var_dump($ftp->chdir('1508'));
     var_dump($ftp->nlist('.'));
 */
-    // Creates a directory
-   // $ftp->mkdir('public_html/1508/test_bo');
-   // $ftp->putAll(dirname(__DIR__), 'public_html/1508/test_bo', FTP_BINARY);
+    
+// Remove the directory and its contents
+    echo 'removing directory ' . $path . PHP_EOL;
+    $ftp->rmdir($path, true);
+    
+    echo 'creating directory ' . $path . PHP_EOL;
+    // Create the directory    
+    $ftp->mkdir($path);
+    
+    echo 'uploading files from ' . $srcPath . ' to ' . $path . PHP_EOL;
+    //Put all files from parent folder
+    $ftp->putAll($srcPath, $path, FTP_BINARY);
+
+    echo 'all done' . PHP_EOL;
