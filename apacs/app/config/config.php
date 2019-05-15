ea0d868ddfbfb9fca944c156e251683437d3d450
Bo Henriksen <stumpdenmark@hotmail.com>
2019-05-15 13:05:58 +0200

    Added config file

---
+++ apacs/app/config/config.php
@@ -0,0 +1,53 @@
+<?php
+
+if(getenv('ENVIRONMENT') == 'DEV'){
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+}
+
+$di->setShared('config', function () {
+	return [
+		"host" => getenv('APACS_DB_HOST'),
+		"port" => getenv('APACS_DB_PORT'),
+		"username" => getenv('APACS_DB_USER'),
+		"password" => getenv('APACS_DB_PASSWORD'),
+		"dbname" => getenv('APACS_DB_DATABASE'),
+		"charset" => getenv('APACS_DB_CHARSET'),
+	];
+});
+
+$di->setShared('solrConfig', function () {
+		return [
+			"scheme" => getenv('SOLR_SCHEME'),
+			"host" => getenv('SOLR_HOST'),
+			"dns" => getenv('SOLR_DNS'),
+			"port" => getenv('SOLR_PORT'),
+			"path" => getenv('SOLR_PATH'),
+			"timeout" => getenv('SOLR_TIMEOUT'),
+			"username" => getenv('SOLR_USERNAME'),
+			"password" => getenv('SOLR_PASSWORD'),
+		];
+});
+
+$di->setShared('cipConfig', function () {
+	return [
+		"host" => getenv('CUMULUS_HOST'),
+		"port" => getenv('CUMULUS_PORT'),
+		"user" => getenv('CUMULUS_USER'),
+		"pass" => getenv('CUMULUS_PASS'),
+		"location" => getenv('CUMULUS_LOCATION'),
+		"catalog" => getenv('CUMULUS_CATALOG'),
+	];
+});
+
+$di->setShared('pageImageLocation', function () {
+	return [
+		'path' => getenv('APACS_IMAGE_PATH'),
+		'type' => getenv('APACS_IMAGE_PROTOCOL'),
+	];
+});
+
+$di->setShared('AccessController', function () use ($di) {
+        $className = getenv('APACS_ACCESS_CTRL_NAME');
+		return new $className($di->get('request'));
+});
