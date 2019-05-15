e43af39e9d4f1750af5163eb6fb0aa2f3031b93c
Bo Henriksen <stumpdenmark@hotmail.com>
2019-05-15 13:44:58 +0200

    Update config.py

--- indexing/config.py
+++ indexing/config.py
@@ -1,82 +1,51 @@
 #! python3
 # -*- coding: utf-8 -*-
 import os
-if os.getenv('PYTHON_ENV', 'DEV') == 'DEV':
+from pathlib import Path  # python3 only
+
+env_path = Path('/etc/.env')
+load_dotenv(dotenv_path=env_path)
+
+if os.getenv('ENVIRONMENT', 'DEV') == 'DEV':
     print("Using development settings.")
-    Config = {
-        "debug" : True,
-        "cumulus" : {
-            #"url": "https://neaonline.dk",
-            "url": "https://cumulus.DAF.local",
-            "port": 8443,
-            "user": "CIP-erindringsbilleder",
-            "password": "***REMOVED***",
-            "catalog": "CIP-erindringsbilleder",
-            "layout": "erindringskatalog"
-        },
-        'polle_db' : {
-            "host": "138.201.87.24",
-            "port": 3306,
-            "user": "politietsregiste",
-            "password": "***REMOVED***",
-            "database": "politietsregisterblade"
-        },
-        "apacs_db" : {
-            "host": "148.251.122.164",
-            "port": 3306,
-            "user":"kbharkiv",
-            "password":"***REMOVED***",
-            "database": "kbharkiv"
-        },
-        "aws_sns" : {
-            "access_key_id": "***REMOVED***",
-            "secret_access_key": "***REMOVED***"
-        },
-        "solr": {
-            "url": "http://solr:8983/solr/apacs_core"
-        },
-        "ftp_kbharkiv": {
-            "url": "phhw-160601.cust.powerhosting.dk",
-            "user": "kbharkiv",
-            "password": "***REMOVED***"
-        }
-    }
 else:
     print("Using production settings!")
-    Config = {
-        "debug" : False,
-        "cumulus" : {
-            "url": "https://cumulus.kbhbilleder.dk",
-            "port": 8443,
-            "user": "CIP-erindringsbilleder",
-            "password": "***REMOVED***",
-            "catalog": "CIP-erindringsbilleder",
-            "layout": "erindringskatalog"
-        },
-        'polle_db' : {
-            "host": "138.201.87.24",
-            "port": 3306,
-            "user": "politietsregiste",
-            "password": "***REMOVED***",
-            "database": "politietsregisterblade"
-        },
-        "apacs_db" : {
-            "host": "148.251.122.164",
-            "port": 3306,
-            "user":"kbharkiv",
-            "password":"***REMOVED***",
-            "database": "kbharkiv"
-        },
-        "aws_sns" : {
-            "access_key_id": "***REMOVED***",
-            "secret_access_key": "***REMOVED***"
-        },
-        "solr": {
-            "url": "http://solr:8983/solr/apacs_core"
-        },
-        "ftp_kbharkiv": {
-            "url": "ftp:kbharkiv.dk",
-            "user": "",
-            "password": ""
-        }
+
+Config = {
+    "debug" : os.getenv("ENVIRONMENT") == 'DEV',
+    "cumulus" : {
+        "url": os.getenv("CUMULUS_HOST"),
+        "port": os.getenv("CUMULUS_PORT"),
+        "user": os.getenv("CUMULUS_USER"),
+        "password": os.getenv("CUMULUS_PASS"),
+        "catalog": os.getenv("CUMULUS_CATALOG"),
+        "layout": os.getenv("CUMULUS_LAYOUT"),
+    },
+    'polle_db' : {
+        "host": os.getenv("POLLE_DB_HOST"),
+        "port": os.getenv("POLLE_DB_PORT"),
+        "user": os.getenv("POLLE_DB_USER"),
+        "password": os.getenv("POLLE_DB_PASSWORD"),
+        "database": os.getenv("POLLE_DB_DATABASE")
+    },
+    "apacs_db" : {
+        "host": os.getenv("APACS_DB_HOST"),
+        "port": os.getenv("APACS_DB_PORT"),
+        "user": os.getenv("APACS_DB_USER"),
+        "password": os.getenv("APACS_DB_PASSWORD"),
+        "database": os.getenv("APACS_DB_DATABASE")
+    },
+    "aws_sns" : {
+        "access_key_id": os.getenv("AWS_SNS_KEY_ID"),
+        "secret_access_key": os.getenv("AWS_SNS_ACCESS_KEY"),
+    },
+    "solr": {
+        "url": os.getenv("SOLR_INTERNAL_URL")
+    },
+    "ftp_kbharkiv": {
+        "url": os.getenv("KBHARKIV_FTP_HOST"),
+        "user": os.getenv("KBHARKIV_FTP_USER"),
+        "password": os.getenv("KBHARKIV_FTP_PASSWORD")
     }
+}
+
