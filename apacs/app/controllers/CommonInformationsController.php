3fcf69395aa5e5fb1f9858e30fd8c36bebe9e784
jfeo <jens@feodor.dk>
2020-02-19 14:53:02 +0100

    Post saving: Only set updated when updating.

--- apacs/app/controllers/CommonInformationsController.php
+++ apacs/app/controllers/CommonInformationsController.php
@@ -407,7 +407,9 @@ class CommonInformationsController extends MainController {
 		$post->height = $input['height'];
 		$post->width = $input['width'];
 		$post->complete = 0;
-		$post->updated = date('Y-m-d H:i:s');
+		if (!is_null($id)) {
+			$post->updated = date('Y-m-d H:i:s');
+		}
 
 		if ($post->ApproximatePostExists()) {
 			$this->response->setStatusCode(403, 'Approximate post already exists');
