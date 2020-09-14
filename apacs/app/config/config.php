41f6138707f6058d116245dd3ae1d019b9bb6cf9
Bo Henriksen <stumpdenmark@hotmail.com>
2020-09-14 15:04:28 +0200

    Update config.php
    Set proper error reporting based on environment

--- apacs/app/config/config.php
+++ apacs/app/config/config.php
@@ -10,8 +10,14 @@ if(file_exists($envLocation . '.env')){
 }
 
 if(getenv('ENVIRONMENT') == 'DEV'){
+	ini_set('error_reporting', E_ALL);
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
+}
+else{
+	error_reporting(0);
+	ini_set('display_errors', 0);
+	ini_set('display_startup_errors', 0);
 }
 
 $di->setShared('config', function () {
