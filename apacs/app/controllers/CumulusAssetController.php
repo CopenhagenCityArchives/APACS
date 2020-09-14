524ea5be683ae77dcb48ff175fbab045d0d3f305
Bo Henriksen <stumpdenmark@hotmail.com>
2020-09-14 22:31:03 +0200

    Use curl when receiving assets

--- apacs/app/controllers/CumulusAssetController.php
+++ apacs/app/controllers/CumulusAssetController.php
@@ -3,42 +3,62 @@
 class CumulusAssetController extends \Phalcon\Mvc\Controller {
 
 	public function AssetDownload($assetId) {
-		try{
-			$url = sprintf("%s:%d/%s/asset/download/%s/%d",
+		$url = sprintf("%s:%d/%s/asset/download/%s/%d",
 			$this->getDI()->get('cipConfig')['host'],
 			$this->getDI()->get('cipConfig')['port'],
 			$this->getDI()->get('cipConfig')['location'],
 			$this->getDI()->get('cipConfig')['catalog'],
 			$assetId
-			);
+		);
 
-			$auth = base64_encode(sprintf("%s:%s",
-				$this->getDI()->get('cipConfig')['user'],
-				$this->getDI()->get('cipConfig')['pass']
-			));
+		return $this->curl_get_contents($url);
+		
+		$auth = base64_encode(sprintf("%s:%s",
+			$this->getDI()->get('cipConfig')['user'],
+			$this->getDI()->get('cipConfig')['pass']
+		));
 
-			// use key 'http' even if you send the request to https://...
-			$options = array(
-				'http' => array(
-					'header'  => array(sprintf("Authorization: Basic %s", $auth)),
-					'method'  => 'GET'
-				),
-				'ssl' => array(
-					'verify_peer' => false
-				)
-			);
-			$context  = stream_context_create($options);
-			$result = @file_get_contents($url, false, $context);
-			if ($result === FALSE) {
-				$this->response->setStatusCode(400, "Invalid Asset ID");
-			} else {
-				header('Content-Type: application/pdf');
-				echo $result;
-			}
+		// use key 'http' even if you send the request to https://...
+		$options = array(
+			'http' => array(
+				'header'  => array(sprintf("Authorization: Basic %s", $auth)),
+				'method'  => 'GET'
+			),
+			'ssl' => array(
+				'verify_peer' => false
+			)
+		);
+		$context  = stream_context_create($options);
+		$result = @file_get_contents($url, false, $context);
+		if ($result === false) {
+			$this->response->setStatusCode(400, "Invalid Asset ID");
+		} else {
+			header('Content-Type: application/pdf');
+			echo $result;
 		}
-		catch(Exception $e){
-			var_dump($e);
-		}
+	}
+
+	private function curl_get_contents($url)
+	{
+	  	$auth = base64_encode(sprintf("%s:%s",
+			$this->getDI()->get('cipConfig')['user'],
+			$this->getDI()->get('cipConfig')['pass']
+		));
+
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, [
+			sprintf("Authorization: Basic %s", $auth)
+		]);
+
+		$data = curl_exec($ch);
+		var_dump($data);
+
+		curl_close($ch);
+	  	return $data;
 	}
 
 }
