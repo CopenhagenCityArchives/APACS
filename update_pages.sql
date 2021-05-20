74517c46a25666c9962dce7a92b8a7f8c68b8164
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2021-05-20 11:38:51 +0200

    update_pages.sql til gennemsyn (skal rebases ud før merge til master)

--- update_pages.sql
+++ update_pages.sql
@@ -1,2 +1,15 @@
-UPDATE apacs_pages SET s3_bucket = 'kbhkilder', s3_key = CONCAT('collections/', relative_filename_converted) WHERE collection_id NOT IN (148, 149);
-UPDATE apacs_pages SET s3_bucket = 'frbkilder', s3_key = relative_filename_converted WHERE collection_id IN (148, 149);
+UPDATE apacs_pages, apacs_units 
+SET
+	apacs_pages.s3_bucket = 'kbhkilder',
+	apacs_pages.s3_key = CONCAT('collections/', relative_filename_converted)
+WHERE 
+	apacs_units.id = apacs_pages.unit_id 
+	AND apacs_units.collection_id NOT IN (148, 149);
+
+UPDATE apacs_pages, apacs_units
+SET
+	apacs_pages.s3_bucket = 'frbkilder',
+	apacs_pages.s3_key = relative_filename_converted
+WHERE
+	apacs_units.id = apacs_pages.unit_id 
+	AND apacs_units.collection_id IN (148, 149);
