20a64c07b810f7e7e99f5e90fa8d64bf4752ba7a
Bo Henriksen <stumpdenmark@hotmail.com>
2021-05-21 12:14:20 +0200

    Update update_pages.sql

--- update_pages.sql
+++ update_pages.sql
@@ -1,15 +1,9 @@
-UPDATE apacs_pages, apacs_units 
-SET
-	apacs_pages.s3_bucket = 'kbhkilder',
-	apacs_pages.s3_key = CONCAT('collections/', relative_filename_converted)
-WHERE 
-	apacs_units.id = apacs_pages.unit_id 
-	AND apacs_units.collection_id NOT IN (148, 149);
+# Opdater s3_key baseret på image_url for filer der allerede ligger i s3
+update apacs_pages set s3_key = SUBSTRING_INDEX(image_url, "/", -1) where s3=1;
+# Opdater s3_key for filer der ikke tidligere lå s3
+update apacs_pages set s3_key = CONCAT('collections/', relative_filename_converted)  where s3=0;
+# Opdater s3_bucket baseret på archive_id
+UPDATE apacs_pages, apacs_units SET apacs_pages.s3_bucket = 'frbkilder' WHERE apacs_units.id = apacs_pages.unit_id AND apacs_units.collection_id IN (148, 149);
+UPDATE apacs_pages, apacs_units SET apacs_pages.s3_bucket = 'kbhkilder' WHERE apacs_units.id = apacs_pages.unit_id AND apacs_units.collection_id NOT IN (148, 149);
 
-UPDATE apacs_pages, apacs_units
-SET
-	apacs_pages.s3_bucket = 'frbkilder',
-	apacs_pages.s3_key = relative_filename_converted
-WHERE
-	apacs_units.id = apacs_pages.unit_id 
-	AND apacs_units.collection_id IN (148, 149);
+# Husk at lave stats.loadTime om til DECIMAL(10,5)
