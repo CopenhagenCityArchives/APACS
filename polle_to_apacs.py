88f845586ecc308baf1c54e71c56c961c5678bff
jfeo <jensfeodor@gmail.com>
2021-05-25 17:56:40 +0200

    Update polle_to_apacs.py
    Insert units with ids starting from a specified point
    - Add parameter to set starting unit id, otherwise ask to start from a unit id based
      on collection id
    - Insert entries and subposts (that nothing else depends on) in
      multi-insert statements

--- polle_to_apacs.py
+++ polle_to_apacs.py
@@ -11,20 +11,24 @@ def image_url(station_number, filmrulle_number, filename):
 
 class UnitInfo:
 
-    def __init__(self, collection_id, description, filmrulle_id):
+    def __init__(self, collection_id, description, filmrulle_id, unit_id=None):
         self.collection_id = collection_id
         self.description = description
         self.filmrulle_id = filmrulle_id
-        self.unit_id = None
+        self.insert_with_id = unit_id is not None
+        self.unit_id = unit_id
         self.pages = 0
     
     def insert(self, mysql):
-        if self.unit_id is not None:
+        if self.unit_id is not None and not self.insert_with_id:
             raise Exception("Unit already inserted")
-
+        
         with mysql.cursor() as cursor:
-            cursor.execute(f"INSERT INTO `apacs_units` (`collections_id`, `description`) VALUES ('{self.collection_id}', '{self.description}');")
-            self.unit_id = cursor.lastrowid
+            if self.insert_with_id:
+                cursor.execute(f"INSERT IGNORE INTO `apacs_units` (`id`, `collections_id`, `description`) VALUES ('{self.unit_id}', '{self.collection_id}', '{self.description}');")
+            else:
+                cursor.execute(f"INSERT INTO `apacs_units` (`collections_id`, `description`) VALUES ('{self.collection_id}', '{self.description}');")
+                self.unit_id = cursor.lastrowid
     
     def update(self, mysql):
         if self.unit_id is None:
@@ -137,6 +141,11 @@ class SubPostInfo:
             cursor.execute(f"INSERT INTO `apacs_subposts` (`pages_id`, `posts_id`, `width`, `height`, `x`, `y`) VALUES ({self.page.page_id}, {self.post.post_id}, 1, 1, 0, 0);")
             self.subpost_id = cursor.lastrowid
 
+    @staticmethod
+    def insertmany(mysql, subposts):
+        with mysql.cursor() as cursor:
+            values = ",".join(map(lambda s: f'({s.page.page_id}, {s.post.post_id}, 1, 1, 0, 0)', subposts))
+            cursor.execute(f"INSERT INTO `apacs_subposts` (`pages_id`, `posts_id`, `width`, `height`, `x`, `y`) VALUES {values};")
 
 class EntryInfo:
 
@@ -154,8 +163,13 @@ class EntryInfo:
             cursor.execute(f"INSERT INTO `apacs_entries` (`tasks_id`, `posts_id`, `users_id`, `concrete_entries_id`, `complete`) VALUES ({self.task_id}, {self.post.post_id}, 0, {self.concrete_entry_id}, 1);")
             self.entry_id = cursor.lastrowid
 
+    @staticmethod
+    def insertmany(mysql, entries):
+        with mysql.cursor() as cursor:
+            values = ",".join(map(lambda e: f'({e.task_id}, {e.post.post_id}, 0, {e.concrete_entry_id}, 1)', entries))
+            cursor.execute(f"INSERT INTO `apacs_entries` (`tasks_id`, `posts_id`, `users_id`, `concrete_entries_id`, `complete`) VALUES {values};")
 
-def generate_units(mysql, collection_id):
+def generate_units(mysql, collection_id, unit_id_generator):
     with mysql.cursor(pymysql.cursors.DictCursor) as cursor:
         cursor.execute("""
             SELECT
@@ -168,7 +182,7 @@ def generate_units(mysql, collection_id):
         """)
 
         for row in cursor.fetchall():
-            yield UnitInfo(collection_id, f"Station {row['station_nummer']} - rulle {row['filmrulle_nummer']}", row['filmrulle_id'])
+            yield UnitInfo(collection_id, f"Station {row['station_nummer']} - rulle {row['filmrulle_nummer']}", row['filmrulle_id'], next(unit_id_generator))
 
 
 def generate_apacs_items(mysql, task_id, unit):
@@ -215,20 +229,45 @@ def generate_apacs_items(mysql, task_id, unit):
     task_unit.update(mysql)
 
 
-def main(task_id, collection_id, mysql):
-    for unit in generate_units(mysql, collection_id):
+def main(task_id, collection_id, start_unit_id, mysql):
+    for unit in generate_units(mysql, collection_id, unit_id_generator(start_unit_id)):
         print(f"Unit (filmrulle id {unit.filmrulle_id})", flush=True, end="\r")
         unit.insert(mysql)
         print(f"Unit (filmrulle id {unit.filmrulle_id}) - {unit.unit_id}", flush=True, end="\r")
+        entries = []
+        subposts = []
+        j = 0
         for i, item in enumerate(generate_apacs_items(mysql, task_id, unit)):
-            if i % 1000 == 0:
-                mysql.commit()
-            item.insert(mysql)
-            print(f"Unit (filmrulle id {unit.filmrulle_id}) - {unit.unit_id} - {i}", flush=True, end="\r")
+            j = i
+            if i % 50 == 0:
+                print(f"Unit (filmrulle id {unit.filmrulle_id}) - {unit.unit_id} - {i}", flush=True, end="\r")
+
+            if isinstance(item, EntryInfo):
+                entries.append(item)
+            elif isinstance(item, SubPostInfo):
+                subposts.append(item)
+            else:
+                item.insert(mysql)
+
+            if len(entries) > 1000:
+                EntryInfo.insertmany(mysql, entries)
+                entries = []
+            if len(subposts) > 1000:
+                SubPostInfo.insertmany(mysql, subposts)
+                subposts = []
+        EntryInfo.insertmany(mysql, entries)
+        SubPostInfo.insertmany(mysql, subposts)
         unit.update(mysql)
-        print()
         mysql.commit()
+        print(f"Unit (filmrulle id {unit.filmrulle_id}) - {unit.unit_id} - {j}", flush=True)
     print()
+
+
+def unit_id_generator(start):
+    i = start
+    while True:
+        yield i
+        i += 1
 
 
 if __name__ == "__main__":
@@ -241,11 +280,17 @@ if __name__ == "__main__":
     parser.add_argument('--db', '-d', type=str, default="apacs")
     parser.add_argument('--user', '-u', type=str, default="root")
     parser.add_argument('--password', '-p', type=str, nargs='?', const=None, default=False)
+    parser.add_argument('--start-unit-id', type=int, default=False)
 
     namespace = parser.parse_args()
 
     if namespace.password is None:
         namespace.password = getpass.getpass()
+    
+    if namespace.start_unit_id == False:
+        namespace.start_unit_id = namespace.collection_id * 100000
+        if input(f'Using starting unit id {namespace.start_unit_id} based on collection id (y/n): ').lower() != 'y':
+            exit(0)
 
     with pymysql.connect(
         host=namespace.host,
@@ -254,4 +299,4 @@ if __name__ == "__main__":
         db=namespace.db,
         charset='utf8'
     ) as mysql:
-        main(namespace.task_id, namespace.collection_id, mysql)
+        main(namespace.task_id, namespace.collection_id, namespace.start_unit_id, mysql)
