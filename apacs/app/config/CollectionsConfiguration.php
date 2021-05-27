6caacb6d6aea595a5f7bfe70045e9f8b8187db23
Bo Henriksen <stumpdenmark@hotmail.com>
2021-05-27 10:12:00 +0200

    Update CollectionsConfiguration.php
    Collections use file endpoint again

--- apacs/app/config/CollectionsConfiguration.php
+++ apacs/app/config/CollectionsConfiguration.php
@@ -25,9 +25,8 @@
 if(!isset($url)){
 	throw new Exception("url is not set, cannot set collection settings!");
 }
- $api_url = $url;
-// For custom collections the CDN is always kbhkilder.kbharkiv.dk
-$cdnUrl = 'https://kbhkilder.kbharkiv.dk/';
+$cdnUrlId = $url . '/file/';
+$cdnUrlPath = $url . '/file?path=/';
 
 $collectionsSettings = array(
 
@@ -43,7 +42,7 @@ $collectionsSettings = array(
 		'long_name' => 'Politiets Mandtal for København 1866 - 1923',
 		'gui_required_fields_text' => 'Vælg minimum gade og år',
 		//How to link the data level objects to images
-		'objects_query' => "select files.id, CONCAT('$cdnUrl', 'collections/mandtal',path) as imageURL, year, month, name FROM files LEFT JOIN volumes ON files.volume_id = volumes.id LEFT JOIN streets on volumes.street_id = streets.id WHERE :query ORDER BY year, month, files.id",
+		'objects_query' => "select files.id, CONCAT('$cdnUrlPath', 'collections/mandtal',path) as imageURL, year, month, name FROM files LEFT JOIN volumes ON files.volume_id = volumes.id LEFT JOIN streets on volumes.street_id = streets.id WHERE :query ORDER BY year, month, files.id",
 		'primary_table_name' => 'files',
 		'starbas_field_name' => false,
 		'levels_type' => 'hierarchy',
@@ -241,7 +240,7 @@ $collectionsSettings = array(
 		'gui_required_fields_text' => 'Vælg en skole for at fortsætte',
 		'primary_table_name' => 'kortteg_files',
 		//How to link the data level objects to images
-		'objects_query' => "SELECT SkoleKildeOpslagId as id, Navn, Kildenavn, kilde.AarstalTil, kilde.AarstalFra, skole.SkoleId as skoleid, skole.navn as skole, opslag.Kildeid as kildeid, kilde.SkoleKildeID as SkoleKildeId, CONCAT('$cdnUrl', 'collections/skoleprotokoller/',FuldFilNavn) as imageURL
+		'objects_query' => "SELECT SkoleKildeOpslagId as id, Navn, Kildenavn, kilde.AarstalTil, kilde.AarstalFra, skole.SkoleId as skoleid, skole.navn as skole, opslag.Kildeid as kildeid, kilde.SkoleKildeID as SkoleKildeId, CONCAT('$cdnUrlPath', 'collections/skoleprotokoller/',FuldFilNavn) as imageURL
                         FROM tblSkoleKildeOpslag as opslag
                         LEFT JOIN tblSkoleKilde as kilde ON opslag.kildeid = kilde.SkoleKildeId
                         LEFT JOIN tblSkole as skole ON kilde.SkoleId = skole.SkoleId
@@ -346,7 +345,7 @@ $collectionsSettings = array(
 		'primary_table_name' => 'apacs_pages',
 		//   'starbas_field_name' => 'starbas_id',
 		//How to link the data level objects to images
-		'objects_query' => "select apacs_pages.id, year, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrl', apacs_pages.s3_key) as imageURL
+		'objects_query' => "select apacs_pages.id, year, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrlId', apacs_pages.id) as imageURL
                         FROM apacs_pages
                         LEFT JOIN begrav_volume ON apacs_pages.volume_id = begrav_volume.id
                         LEFT JOIN begrav_volume_years ON begrav_volume.id = begrav_volume_years.volume_id
@@ -427,7 +426,7 @@ $collectionsSettings = array(
 		'image_type' => 'image',
 		'primary_table_name' => 'apacs_pages',
 		//How to link the data level objects to images
-		'objects_query' => "SELECT DISTINCT apacs_pages.id, riv_1, sex, apacs_pages.starbas_id, nicetitle, CONCAT('$cdnUrl', apacs_pages.s3_key) as imageURL
+		'objects_query' => "SELECT DISTINCT apacs_pages.id, riv_1, sex, apacs_pages.starbas_id, nicetitle, CONCAT('$cdnUrlId', apacs_pages.id) as imageURL
                         FROM apacs_pages
                         LEFT JOIN begrav_volume ON apacs_pages.volume_id = begrav_volume.id
                         WHERE volumetype_id = 2 AND is_public = 1 AND :query ORDER BY apacs_pages.page_number",
@@ -524,7 +523,7 @@ $collectionsSettings = array(
 		'primary_table_name' => 'apacs_pages',
 		//   'starbas_field_name' => 'starbas_id',
 		//How to link the data level objects to images
-		'objects_query' => "select apacs_pages.id, riv_1, apacs_pages.starbas_id, CONCAT('$cdnUrl', apacs_pages.s3_key) as imageURL
+		'objects_query' => "select apacs_pages.id, riv_1, apacs_pages.starbas_id, CONCAT('$cdnUrlId', apacs_pages.id) as imageURL
                         FROM apacs_pages
                         LEFT JOIN begrav_volume ON apacs_pages.volume_id = begrav_volume.id
                         WHERE volumetype_id = 4 AND is_public = 1 AND :query ORDER BY apacs_pages.page_number",
@@ -589,7 +588,7 @@ $collectionsSettings = array(
 		'image_type' => 'image',
 		'primary_table_name' => 'apacs_pages',
 		//How to link the data level objects to images
-		'objects_query' => "select DISTINCT apacs_pages.id, riv_1, apacs_pages.starbas_id, nicetitle, CONCAT('$cdnUrl', apacs_pages.s3_key) as imageURL
+		'objects_query' => "select DISTINCT apacs_pages.id, riv_1, apacs_pages.starbas_id, nicetitle, CONCAT('$cdnUrlId', apacs_pages.id) as imageURL
                         FROM apacs_pages
                         LEFT JOIN begrav_volume ON apacs_pages.volume_id = begrav_volume.id
                         WHERE volumetype_id = 3 AND is_public = 1 AND :query ORDER BY page_number",
@@ -670,7 +669,7 @@ $collectionsSettings = array(
 		'primary_table_name' => 'apacs_pages',
 		//   'starbas_field_name' => 'starbas_id',
 		//How to link the data level objects to images
-		'objects_query' => "select apacs_pages.id, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrl', apacs_pages.s3_key) as imageURL
+		'objects_query' => "select apacs_pages.id, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrlId', apacs_pages.id) as imageURL
                         FROM apacs_pages
                         LEFT JOIN begrav_volume ON apacs_pages.volume_id = begrav_volume.id
                         WHERE volumetype_id = 6 AND is_public = 1 AND :query",
@@ -736,7 +735,7 @@ $collectionsSettings = array(
 		'primary_table_name' => 'apacs_pages',
 		//   'starbas_field_name' => 'starbas_id',
 		//How to link the data level objects to images
-		'objects_query' => "select apacs_pages.id, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrl', apacs_pages.s3_key) as imageURL
+		'objects_query' => "select apacs_pages.id, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrlId', apacs_pages.id) as imageURL
                         FROM apacs_pages
                         LEFT JOIN begrav_volume ON apacs_pages.volume_id = begrav_volume.id
                         WHERE volumetype_id = 5 AND is_public = 1 AND :query",
@@ -802,7 +801,7 @@ $collectionsSettings = array(
 		'primary_table_name' => 'apacs_pages',
 		//   'starbas_field_name' => 'starbas_id',
 		//How to link the data level objects to images
-		'objects_query' => "select apacs_pages.id, creator_name, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrl', apacs_pages.s3_key) as imageURL
+		'objects_query' => "select apacs_pages.id, creator_name, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrlId', apacs_pages.id) as imageURL
                         FROM apacs_pages
                         LEFT JOIN begrav_volume ON apacs_pages.volume_id = begrav_volume.id
                         WHERE volumetype_id = 7 AND is_public = 1 AND :query",
@@ -884,7 +883,7 @@ $collectionsSettings = array(
 		'primary_table_name' => 'apacs_pages',
 		//   'starbas_field_name' => 'starbas_id',
 		//How to link the data level objects to images
-		'objects_query' => "select apacs_pages.id, creator_name, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrl', apacs_pages.s3_key) as imageURL
+		'objects_query' => "select apacs_pages.id, creator_name, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrlId', apacs_pages.id) as imageURL
                         FROM apacs_pages
                         LEFT JOIN begrav_volume ON apacs_pages.volume_id = begrav_volume.id
                         WHERE volumetype_id = 8 AND is_public = 1 AND :query",
@@ -950,7 +949,7 @@ $collectionsSettings = array(
 		'primary_table_name' => 'apacs_pages',
 		//   'starbas_field_name' => 'starbas_id',
 		//How to link the data level objects to images
-		'objects_query' => "select apacs_pages.id, creator_name, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrl', apacs_pages.s3_key) as imageURL
+		'objects_query' => "select apacs_pages.id, creator_name, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrlId', apacs_pages.id) as imageURL
                         FROM apacs_pages
                         LEFT JOIN begrav_volume ON apacs_pages.volume_id = begrav_volume.id
                         WHERE volumetype_id = 9 AND is_public = 1 AND :query",
@@ -1016,7 +1015,7 @@ $collectionsSettings = array(
 		'primary_table_name' => 'apacs_pages',
 		//   'starbas_field_name' => 'starbas_id',
 		//How to link the data level objects to images
-		'objects_query' => "select apacs_pages.id, creator_name, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrl', apacs_pages.s3_key) as imageURL
+		'objects_query' => "select apacs_pages.id, creator_name, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrlId', apacs_pages.id) as imageURL
                         FROM apacs_pages
                         LEFT JOIN begrav_volume ON apacs_pages.volume_id = begrav_volume.id
                         WHERE volumetype_id = 10 AND is_public = 1 AND :query",
@@ -1082,7 +1081,7 @@ $collectionsSettings = array(
 		'primary_table_name' => 'apacs_pages',
 		//   'starbas_field_name' => 'starbas_id',
 		//How to link the data level objects to images
-		'objects_query' => "SELECT apacs_pages.id, year, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrl', apacs_pages.s3_key) as imageURL
+		'objects_query' => "SELECT apacs_pages.id, year, nicetitle, apacs_pages.starbas_id, CONCAT('$cdnUrlId', apacs_pages.id) as imageURL
                         FROM apacs_pages
                         LEFT JOIN begrav_volume ON apacs_pages.volume_id = begrav_volume.id
                         LEFT JOIN begrav_volume_years ON begrav_volume.id = begrav_volume_years.volume_id
@@ -1148,7 +1147,7 @@ $collectionsSettings = array(
 		'image_type' => 'image',
 		'primary_table_name' => 'apacs_pages',
 		//How to link the data level objects to images
-		'objects_query' => "SELECT DISTINCT apacs_pages.id, riv_1, sex, apacs_pages.starbas_id, nicetitle, CONCAT('$cdnUrl', apacs_pages.s3_key) as imageURL
+		'objects_query' => "SELECT DISTINCT apacs_pages.id, riv_1, sex, apacs_pages.starbas_id, nicetitle, CONCAT('$cdnUrlId', apacs_pages.id) as imageURL
                         FROM apacs_pages
                         LEFT JOIN begrav_volume ON apacs_pages.volume_id = begrav_volume.id
                         WHERE volumetype_id = 12 AND is_public = 1 AND :query",
